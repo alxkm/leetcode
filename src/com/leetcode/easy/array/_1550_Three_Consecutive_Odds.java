9dae81bd815daa6a6465c6065786675560292bc0
Oleksandr Klymenko <okly@telenor.dk>
2022-10-31 21:40:18 +0200

    https://leetcode.com/problems/three-consecutive-odds/

---
+++ src/com/leetcode/easy/array/_1550_Three_Consecutive_Odds.java
@@ -0,0 +1,16 @@
+package com.leetcode.easy.array;
+
+public class _1550_Three_Consecutive_Odds {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        if (arr.length <= 2) return false;
+        int a = arr[0] + arr[1] + arr[2];
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i - 2] % 2 != 0
+                    && arr[i - 1] % 2 != 0
+                    && arr[i] % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
