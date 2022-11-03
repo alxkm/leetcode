1611d59b2ba6f09aaf118652fcf8069717a82f03
Oleksandr Klymenko <okly@telenor.dk>
2022-11-03 11:34:55 +0200

    https://leetcode.com/problems/merge-nodes-in-between-zeros/

--- src/com/leetcode/easy/list/ListNode.java
+++ src/com/leetcode/easy/list/ListNode.java
@@ -1,9 +1,9 @@
 package com.leetcode.easy.list;
 
 public class ListNode {
-      int val;
+      public int val;
       public ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      public ListNode() {}
+      public ListNode(int val) { this.val = val; }
+      public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
---
+++ src/com/leetcode/easy/simulation/_2181_Merge_Nodes_in_Between_Zeros.java
@@ -0,0 +1,39 @@
+package com.leetcode.easy.simulation;
+
+import com.leetcode.easy.list.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _2181_Merge_Nodes_in_Between_Zeros {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode l = head;
+        List<Integer> list = new ArrayList<>();
+        int zero = 0;
+        int sum = 0;
+        while (l != null) {
+            if (l.val == 0) {
+                zero++;
+                if (zero == 2) {
+                    if (sum != 0) {
+                        list.add(sum);
+                        sum = 0;
+                    }
+                    zero--;
+                }
+            } else if (zero == 1){
+                sum += l.val;
+            }
+            l = l.next;
+        }
+        if (list.size() == 0) return null;
+        ListNode a = new ListNode();
+        head = a;
+        for (int n : list) {
+            a.next = new ListNode();
+            a = a.next;
+            a.val = n;
+        }
+        return head.next;
+    }
+}
