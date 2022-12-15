f55aef22d1f0dc673616e985af8fb17a1f7ec1e9
Oleksandr Klymenko <okly@telenor.dk>
2022-12-15 23:54:09 +0200

    https://leetcode.com/problems/product-of-array-except-self/description/

---
+++ src/com/leetcode/medium/array/_238_Product_of_Array_Except_Self.java
@@ -0,0 +1,30 @@
+package com.leetcode.medium.array;
+
+import java.util.Arrays;
+
+public class _238_Product_of_Array_Except_Self {
+    static public int[] productExceptSelf(int[] nums) {
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        int[] answ = new int[nums.length];
+        prefix[0] = nums[0];
+        suffix[0] = nums[nums.length - 1];
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] * nums[i];
+        }
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] * nums[nums.length - i - 1];
+        }
+        answ[0] = suffix[1];
+        answ[nums.length - 1] = prefix[nums.length - 2];
+        for (int i = 1; i < nums.length - 1; i++) {
+            answ[i] = prefix[i - 1] + suffix[i + 1];
+        }
+        return answ;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4};
+        System.out.println(Arrays.toString(productExceptSelf(a)));
+    }
+}
