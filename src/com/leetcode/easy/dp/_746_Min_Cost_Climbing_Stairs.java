bb8d832d07a206aed3d2b7659ca705fdded0c0b9
Alex Klm <alexanderklmn@gmail.com>
2022-12-26 10:55:43 +0200

    https://leetcode.com/problems/min-cost-climbing-stairs/
    Co-authored-by: Oleksandr Klymenko <okly@telenor.dk>

---
+++ src/com/leetcode/easy/dp/_746_Min_Cost_Climbing_Stairs.java
@@ -0,0 +1,33 @@
+package com.leetcode.easy.dp;
+
+public class _746_Min_Cost_Climbing_Stairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+    }
+
+    public int minCostClimbingStairs1(int[] cost) {
+        int min = 0;
+        for (int i = 0; i < cost.length - 1; i++) {
+            int first = cost[i] + ((i + 2) <= cost.length - 1 ? cost[i + 2] : 0);
+            int second = cost[i + 1] + ((i + 3) <= cost.length - 1 ? cost[i + 3] : 0);
+            if (first < second) {
+                i += 2;
+                min += first;
+                System.out.println("select first " + first);
+            } else {
+                i += 3;
+                min += second;
+                System.out.println("select second " + second);
+            }
+            System.out.println(i);
+            if (i + 1 >= cost.length) break;
+        }
+        return min;
+    }
+}
