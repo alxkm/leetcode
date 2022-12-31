338e29db88e6e64bf64692517ffb09af1646f75d
Oleksandr Klymenko <okly@telenor.dk>
2022-12-31 20:01:02 +0200

    https://leetcode.com/problems/ugly-number/

---
+++ src/com/leetcode/easy/math/_263_Ugly_Number.java
@@ -0,0 +1,14 @@
+package com.leetcode.easy.math;
+
+public class _263_Ugly_Number {
+    public boolean isUgly(int n) {
+        while (true) {
+            if (n == 0) return false;
+            if (n == 1) return true;
+            if (n % 2 == 0) n /= 2;
+            else if (n % 3 == 0) n /= 3;
+            else if (n % 5 == 0) n /= 5;
+            else return false;
+        }
+    }
+}
