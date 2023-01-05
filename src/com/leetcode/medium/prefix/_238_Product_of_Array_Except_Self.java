0edee20cc028dd47f256b19c80c10ff9fe65ad8a
Alex Klm <alexanderklmn@gmail.com>
2023-01-05 15:54:49 +0200

    https://leetcode.com/problems/product-of-array-except-self/
    Co-authored-by: Oleksandr Klymenko <okly@telenor.dk>

---
+++ src/com/leetcode/medium/prefix/_238_Product_of_Array_Except_Self.java
@@ -0,0 +1,23 @@
+package com.leetcode.medium.prefix;
+
+public class _238_Product_of_Array_Except_Self {
+    public int[] productExceptSelf(int[] nums) {
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        int[] answ = new int[nums.length];
+        prefix[0] = nums[0];
+        suffix[nums.length - 1] = nums[nums.length - 1];
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] * nums[i];
+        }
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i];
+        }
+        answ[0] = suffix[1];
+        answ[nums.length - 1] = prefix[nums.length - 2];
+        for (int i = 1; i < nums.length - 1; i++) {
+            answ[i] = prefix[i - 1] * suffix[i + 1];
+        }
+        return answ;
+    }
+}
