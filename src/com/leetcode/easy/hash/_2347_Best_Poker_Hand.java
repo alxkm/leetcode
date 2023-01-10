6ab27898f5d0dec97dd4998248a46b1731dc100c
Oleksandr Klymenko <okly@telenor.dk>
2023-01-10 11:38:48 +0200

    https://leetcode.com/problems/best-poker-hand/

---
+++ src/com/leetcode/easy/hash/_2347_Best_Poker_Hand.java
@@ -0,0 +1,33 @@
+package com.leetcode.easy.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _2347_Best_Poker_Hand {
+    public String bestHand(int[] ranks, char[] suits) {
+        Map<Character, Integer> s = new HashMap<>();
+        Map<Integer, Integer> r = new HashMap<>();
+
+        for (char suit : suits) {
+            s.put(suit, s.getOrDefault(suit, 0) + 1);
+        }
+
+        for (int rank : ranks) {
+            r.put(rank, r.getOrDefault(rank, 0) + 1);
+        }
+
+        for (var e : s.entrySet()) {
+            if (e.getValue() == 5) return "Flush";
+        }
+
+        for (var e : r.entrySet()) {
+            if (e.getValue() >= 3) return "Three of a Kind";
+        }
+
+        for (var e : r.entrySet()) {
+            if (e.getValue() == 2) return "Pair";
+        }
+
+        return "High Card";
+    }
+}
