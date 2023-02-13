b3f1c3aeda540ca513fdd085b58dbbe517e26f44
Oleksandr Klymenko <okly@telenor.dk>
2023-02-13 13:45:29 +0200

    https://leetcode.com/problems/arranging-coins/

---
+++ src/com/leetcode/easy/greedly/_441_Arranging_Coins.java
@@ -0,0 +1,18 @@
+package com.leetcode.easy.greedly;
+
+public class _441_Arranging_Coins {
+    public int arrangeCoins(int n) {
+        int q = 0;
+        int i = 1;
+        while (true) {
+            if (n - i >= 0) {
+                q++;
+                n -= i;
+                i++;
+                continue;
+            }
+            break;
+        }
+        return q;
+    }
+}
