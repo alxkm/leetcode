efc159b77965e7bcc2c49ecb63ee85fbd1870623
Oleksandr Klymenko <okly@telenor.dk>
2023-02-14 14:42:41 +0200

    https://leetcode.com/problems/subrectangle-queries/

---
+++ src/com/leetcode/top/_1476_Subrectangle_Queries.java
@@ -0,0 +1,33 @@
+package com.leetcode.top;
+
+public class _1476_Subrectangle_Queries {
+    //https://leetcode.com/problems/subrectangle-queries/
+    //1476. Subrectangle Queries
+
+    class SubrectangleQueries {
+        private int[][] r;
+
+        public SubrectangleQueries(int[][] rectangle) {
+            this.r = rectangle;
+        }
+
+        public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+            for (int i = row1; i <= row2; i++) {
+                for (int j = col1; j <= col2; j++) {
+                    r[i][j] = newValue;
+                }
+            }
+        }
+
+        public int getValue(int row, int col) {
+            return r[row][col];
+        }
+    }
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries obj = new SubrectangleQueries(rectangle);
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj.getValue(row,col);
+ */
+}
