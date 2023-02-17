769e9427d25e2860db5ea12d1f2eb65c0bf6d2ca
Alex Klm <alexanderklmn@gmail.com>
2023-02-17 21:16:14 +0200

    https://leetcode.com/problems/group-anagrams/
    Co-authored-by: Oleksandr Klymenko <okly@telenor.dk>

---
+++ src/com/leetcode/top/_49_Group_Anagrams.java
@@ -0,0 +1,29 @@
+package com.leetcode.top;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class _49_Group_Anagrams {
+    //https://leetcode.com/problems/group-anagrams/
+    //49. Group Anagrams
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str: strs) {
+            char[] newArray = Arrays.copyOf(str.toCharArray(), str.toCharArray().length);
+            Arrays.sort(newArray);
+            StringBuilder sb = new StringBuilder();
+            for (char ch: newArray) {
+                sb.append(ch);
+            }
+            var list = map.getOrDefault(sb.toString(), new ArrayList<String>());
+            list.add(str);
+            map.put(sb.toString(), list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
