b6a58e307ef38ff573ce46e9f3b2ee7bc8693c4c
Oleksandr Klymenko <okly@telenor.dk>
2023-02-17 21:30:24 +0200

    https://leetcode.com/problems/remove-nth-node-from-end-of-list/

---
+++ src/com/leetcode/top/list/_19_Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,41 @@
+package com.leetcode.top.list;
+
+public class _19_Remove_Nth_Node_From_End_of_List {
+    //https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+    //19. Remove Nth Node From End of List
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode copy = head;
+        int size = 0;
+        while (copy != null) {
+            size++;
+            copy = copy.next;
+        }
+        if (n == 1 && size == 1) return null;
+        if (n == size) {
+            head = head.next;
+            return head;
+        }
+        int left = size - n;
+        copy = head;
+        int i = 1;
+        while (i < left) {
+            i++;
+            copy = copy.next;
+        }
+        // System.out.println(copy.val);
+        // System.out.println(n);
+        // System.out.println(i);
+        if (copy.next != null)
+            copy.next = copy.next.next;
+        return head;
+    }
+}
