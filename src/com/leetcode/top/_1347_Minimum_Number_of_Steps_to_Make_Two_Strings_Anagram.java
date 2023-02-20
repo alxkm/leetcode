4d744301926d2cee3c89dc64a5f408132f327d5c
Oleksandr Klymenko <okly@telenor.dk>
2023-02-20 15:50:06 +0200

    https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/

---
+++ src/com/leetcode/top/_1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.java
@@ -0,0 +1,30 @@
+package com.leetcode.top;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _1347_Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram {
+    //https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+    //1347. Minimum Number of Steps to Make Two Strings Anagram
+
+    public int minSteps(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch: t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        int size = 0;
+
+        for (char ch: s.toCharArray()) {
+            var num = map.get(ch);
+
+            if (num == null || num == 0) {
+                size++;
+            } else {
+                map.put(ch, num - 1);
+            }
+        }
+
+        return size;
+    }
+}
