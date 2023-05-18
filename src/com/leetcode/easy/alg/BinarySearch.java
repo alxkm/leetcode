3076bc15e7120a9c24c2f1483c1884258a8cd15a
Alex Klm <alexanderklmn@gmail.com>
2023-05-18 17:01:10 +0300

    Fix binary search

--- src/com/leetcode/easy/alg/BinarySearch.java
+++ src/com/leetcode/easy/alg/BinarySearch.java
@@ -18,8 +18,7 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,6,7,8,9};
-
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         System.out.println(binarySearch(a, 4));
     }
 }
