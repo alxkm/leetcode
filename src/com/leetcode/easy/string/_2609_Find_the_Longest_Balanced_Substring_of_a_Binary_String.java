b83a6fbb4cb4a97e576ff2802d04001f7959a3e7
Alex Klm <alexanderklmn@gmail.com>
2023-06-22 17:13:32 +0300

    https://leetcode.com/problems/find-the-longest-balanced-substring-of-a-binary-string/

---
+++ src/com/leetcode/easy/string/_2609_Find_the_Longest_Balanced_Substring_of_a_Binary_String.java
@@ -0,0 +1,25 @@
+package com.leetcode.easy.string;
+
+public class _2609_Find_the_Longest_Balanced_Substring_of_a_Binary_String {
+    //https://leetcode.com/problems/find-the-longest-balanced-substring-of-a-binary-string/
+
+    public int findTheLongestBalancedSubstring(String s) {
+        int max = 0;
+        for (int i = 0; i < s.length(); ) {
+            int z = 0;
+            int o = 0;
+            while (i < s.length() && s.charAt(i) == '0') {
+                z++;
+                i++;
+            }
+            while (i < s.length() && s.charAt(i) == '1' && z > 0) {
+                o++;
+                i++;
+                z--;
+                max = Math.max(max, o * 2);
+            }
+            while (i < s.length() && s.charAt(i) == '1') i++;
+        }
+        return max;
+    }
+}
