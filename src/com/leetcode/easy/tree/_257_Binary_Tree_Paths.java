5f03ae074da9be11815887cb1975f371ca7308d4
Alex Klm <alexanderklmn@gmail.com>
2023-06-24 15:29:45 +0300

    https://leetcode.com/problems/binary-tree-paths/

---
+++ src/com/leetcode/easy/tree/_257_Binary_Tree_Paths.java
@@ -0,0 +1,28 @@
+package com.leetcode.easy.tree;
+
+import com.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _257_Binary_Tree_Paths {
+    //https://leetcode.com/problems/binary-tree-paths/
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) return null;
+        List<String> list = new ArrayList<>();
+        dfs(root, "", list);
+        return list;
+    }
+
+    private void dfs(TreeNode t, String s, List<String> list) {
+        if (t == null) return;
+        if (t.left == null && t.right == null) {
+            list.add(s + t.val);
+            return;
+        } else {
+            dfs(t.left, s + t.val + "->", list);
+            dfs(t.right, s + t.val + "->", list);
+        }
+    }
+}
