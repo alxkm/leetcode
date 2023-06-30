3c03f8240bbd3dc41e35047f9b82ab6f5bdbd00e
Alex Klm <alexanderklmn@gmail.com>
2023-06-30 18:08:52 +0300

    https://leetcode.com/problems/h-index/

---
+++ src/com/leetcode/medium/array/_274_H_Index.java
@@ -0,0 +1,18 @@
+package com.leetcode.medium.array;
+
+import java.util.Arrays;
+
+public class _274_H_Index {
+    //https://leetcode.com/problems/h-index/
+
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int max = 0, n = citations.length;
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= (n - i)) {
+                max = Math.max(max, n - i);
+            }
+        }
+        return max;
+    }
+}
