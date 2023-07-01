9ed5a4d41293bf88018d0ebf7a2690d80fe28bd0
Alex Klm <alexanderklmn@gmail.com>
2023-07-01 13:57:38 +0300

    https://leetcode.com/problems/longest-increasing-subsequence/

---
+++ src/com/leetcode/medium/dp/_300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,21 @@
+package com.leetcode.medium.dp;
+
+public class _300_Longest_Increasing_Subsequence {
+    //https://leetcode.com/problems/longest-increasing-subsequence/
+
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int max = 1;
+        dp[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
