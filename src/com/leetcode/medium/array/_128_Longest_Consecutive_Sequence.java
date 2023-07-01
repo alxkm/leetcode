b9e0da45ab91b4023cc327829754cde9b8618297
Oleksandr Klymenko <okly@telenor.dk>
2023-07-01 13:39:08 +0300

    https://leetcode.com/problems/longest-consecutive-sequence/

--- src/com/leetcode/medium/array/_128_Longest_Consecutive_Sequence.java
+++ src/com/leetcode/medium/array/_128_Longest_Consecutive_Sequence.java
@@ -27,4 +27,26 @@ public class _128_Longest_Consecutive_Sequence {
         }
         return counter;
     }
+
+    public int longestConsecutive1(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        Set<Integer> set = new HashSet<>();
+        for (int n: nums) set.add(n);
+        int max = 1;
+        for (int n: nums) {
+            int localMax = 1;
+            int start = n;
+            while (set.contains(--start)) {
+                set.remove(start);
+                localMax++;
+            }
+            start = n;
+            while (set.contains(++start)) {
+                set.remove(start);
+                localMax++;
+            }
+            max = Math.max(max, localMax);
+        }
+        return max;
+    }
 }
