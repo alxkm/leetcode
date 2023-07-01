e0d4935046516900fe3d2694e255a198df716f7a
Alex Klm <alexanderklmn@gmail.com>
2023-07-01 13:55:09 +0300

    https://leetcode.com/problems/pascals-triangle-ii/

--- src/com/leetcode/easy/array/_119_Pascal_s_Triangle_II.java
+++ src/com/leetcode/easy/array/_119_Pascal_s_Triangle_II.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _119_Pascal_s_Triangle_II {
+    //https://leetcode.com/problems/pascals-triangle-ii/
+
     static public List<Integer> getRow(int rowIndex) {
         List<Integer> list = new ArrayList<>(rowIndex  + 1);
         if (rowIndex == 0) return List.of(1);
@@ -26,6 +28,23 @@ public class _119_Pascal_s_Triangle_II {
             }
         }
         return list;
+    }
+
+    public List<Integer> getRow1(int rowIndex) {
+        if (rowIndex == 0) return List.of(1);
+        List<Integer> l = new ArrayList<>(rowIndex + 1);
+        l.add(1);
+        l.add(1);
+        for (int i = 1; i < rowIndex; i++) {
+            int prev = l.get(0);
+            for (int j = 1; j <= i; j++) {
+                int cur = l.get(j) + prev;
+                prev = l.get(j);
+                l.set(j, cur);
+            }
+            l.add(1);
+        }
+        return l;
     }
 
     public static void main(String[] args) {
