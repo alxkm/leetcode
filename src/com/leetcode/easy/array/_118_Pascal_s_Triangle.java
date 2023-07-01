aab8628ae0b8a77a228e08d769bd2e6bb25f9661
Alex Klm <alexanderklmn@gmail.com>
2023-07-01 13:54:15 +0300

    https://leetcode.com/problems/pascals-triangle/

--- src/com/leetcode/easy/array/_118_Pascal_s_Triangle.java
+++ src/com/leetcode/easy/array/_118_Pascal_s_Triangle.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _118_Pascal_s_Triangle {
+    //https://leetcode.com/problems/pascals-triangle/
+
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> list = new ArrayList<>();
         list.add(List.of(1));
@@ -24,5 +26,24 @@ public class _118_Pascal_s_Triangle {
             list.add(temp);
         }
         return list;
+    }
+
+
+    public List<List<Integer>> generate1(int numRows) {
+        List<List<Integer>> ll = new ArrayList<>();
+        ll.add(List.of(1));
+        if (numRows == 1) return ll;
+        ll.add(List.of(1, 1));
+        for (int i = 2; i < numRows; i++) {
+            List<Integer> l = new ArrayList<Integer>(i + 1);
+            l.add(1);
+            for (int j = 1; j < i; j++) {
+                List<Integer> prev = ll.get(i - 1);
+                l.add(prev.get(j - 1) + prev.get(j));
+            }
+            l.add(1);
+            ll.add(l);
+        }
+        return ll;
     }
 }
