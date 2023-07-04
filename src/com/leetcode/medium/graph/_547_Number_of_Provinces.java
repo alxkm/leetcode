eae64066dbafc658af8240ba39567fda3911743f
Alex Klm <alexanderklmn@gmail.com>
2023-07-04 10:58:57 +0300

    https://leetcode.com/problems/number-of-provinces/

---
+++ src/com/leetcode/medium/graph/_547_Number_of_Provinces.java
@@ -0,0 +1,36 @@
+package com.leetcode.medium.graph;
+
+public class _547_Number_of_Provinces {
+    //https://leetcode.com/problems/number-of-provinces/
+
+    private boolean[] visit;
+    private int m = 0;
+    private int n = 0;
+
+    public int findCircleNum(int[][] isConnected) {
+        visit = new boolean[isConnected.length];
+        int countriesCounter = 0;
+        m = isConnected.length;
+        n = isConnected[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (!visit[i]) {
+                countriesCounter++;
+                dfs(isConnected, i);
+            }
+        }
+        return countriesCounter;
+    }
+
+    private void dfs(int[][] isConnected, int i) {
+        if (i < 0 || i >= m || visit[i]) {
+            return;
+        }
+        visit[i] = true;
+        for (int j = 0; j < n; j++) {
+            if (isConnected[i][j] == 1 && !visit[j]) {
+                dfs(isConnected, j);
+            }
+        }
+    }
+}
