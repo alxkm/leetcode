d72f304ad9bc480af9d046a46e46a3ab7060ac04
Alex Klm <alexanderklmn@gmail.com>
2023-07-06 18:28:54 +0300

    https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/

---
+++ src/com/leetcode/medium/backtrack/_1593_Split_a_String_Into_the_Max_Number_of_Unique_Substrings.java
@@ -0,0 +1,30 @@
+package com.leetcode.medium.backtrack;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _1593_Split_a_String_Into_the_Max_Number_of_Unique_Substrings {
+    //https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/
+
+    private int max = 0;
+
+    public int maxUniqueSplit(String s) {
+        f(s, 0, new HashSet<>());
+        return max;
+    }
+
+    private void f(String s, int index, Set<String> set) {
+        if (index >= s.length()) {
+            max = Math.max(max, set.size());
+            return;
+        }
+
+        for (int i = index + 1; i <= s.length(); i++) {
+            String str = s.substring(index, i);
+            if (set.add(str)) {
+                f(s, i, set);
+                set.remove(str);
+            }
+        }
+    }
+}
