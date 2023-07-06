ff201c8a2eeca3f61f85a571283a590fab1f208f
Oleksandr Klymenko <okly@telenor.dk>
2023-07-06 18:21:31 +0300

    Fix tree util

--- src/com/leetcode/TreeUtil.java
+++ src/com/leetcode/TreeUtil.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import com.leetcode.top.Node;
+
 public class TreeUtil {
     public static TreeNode formTree(Integer[] data) {
         TreeNode root = new TreeNode(data[0]);
@@ -19,6 +21,29 @@ public class TreeUtil {
         int rightIndex = 2 * rootIndex + 2;
         if (rightIndex < n && null != data[rightIndex]) {
             root.right = new TreeNode(data[rightIndex]);
+            form(root.right, rightIndex, data);
+        }
+    }
+
+
+    public static Node formTreeNodeConnected(Integer[] data) {
+        Node root = new Node(data[0]);
+        form(root, 0, data);
+        return root;
+    }
+
+    private static void form(Node root, int rootIndex, Integer[] data) {
+        int n = data.length;
+
+        int leftIndex = 2 * rootIndex + 1;
+        if (leftIndex < n && null != data[leftIndex]) {
+            root.left = new Node(data[leftIndex]);
+            form(root.left, leftIndex, data);
+        }
+
+        int rightIndex = 2 * rootIndex + 2;
+        if (rightIndex < n && null != data[rightIndex]) {
+            root.right = new Node(data[rightIndex]);
             form(root.right, rightIndex, data);
         }
     }
