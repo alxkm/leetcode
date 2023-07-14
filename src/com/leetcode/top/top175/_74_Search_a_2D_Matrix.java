b6fea9e1ff221bdcd5852b4cb1ae93b9ab191b3b
Oleksandr Klymenko <okly@telenor.dk>
2023-07-14 13:26:05 +0300

    https://leetcode.com/problems/search-a-2d-matrix/description/

---
+++ src/com/leetcode/top/top175/_74_Search_a_2D_Matrix.java
@@ -0,0 +1,28 @@
+package com.leetcode.top.top175;
+
+public class _74_Search_a_2D_Matrix {
+    //https://leetcode.com/problems/search-a-2d-matrix/description/
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int[] row: matrix) {
+            if (row[0] <= target && row[row.length - 1] >= target) {
+                int l = 0;
+                int r = row.length - 1;
+                if (row[l] == target || row[r] == target) return true;
+
+                while (l + 1 < r) {
+                    int m = (l + r) / 2;
+                    if (row[m] == target) return true;
+
+                    if (row[m] < target) {
+                        l = m;
+                    } else {
+                        r = m;
+                    }
+                }
+                if (row[l] == target) return true;
+            }
+        }
+        return false;
+    }
+}
