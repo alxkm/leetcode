b518cf92561a5593ed995f0618186d15c87bd791
Oleksandr Klymenko <okly@telenor.dk>
2023-07-14 13:21:57 +0300

    https://leetcode.com/problems/binary-tree-level-order-traversal/

---
+++ src/com/leetcode/medium/tree/_102_Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,36 @@
+package com.leetcode.medium.tree;
+
+import com.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class _102_Binary_Tree_Level_Order_Traversal {
+    //https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> nodes = new ArrayList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            Queue<TreeNode> newQueue = new LinkedList<>();
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.remove();
+                list.add(node.val);
+                if (node.left != null) {
+                    newQueue.add(node.left);
+                }
+                if (node.right != null) {
+                    newQueue.add(node.right);
+                }
+            }
+            queue = newQueue;
+            nodes.add(list);
+        }
+        return nodes;
+    }
+}
