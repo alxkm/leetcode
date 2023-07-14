de9e30634628eb8471def1f55ac1a87e0b1f7430
Alex Klm <alexanderklmn@gmail.com>
2023-07-14 13:25:21 +0300

    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/

---
+++ src/com/leetcode/medium/tree/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,21 @@
+package com.leetcode.medium.tree;
+
+import com.leetcode.TreeNode;
+
+public class _235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        var cur = root;
+        while (cur != null) {
+            if (p.val > cur.val && q.val > cur.val) {
+                cur = cur.right;
+            } else if (p.val < cur.val && q.val < cur.val) {
+                cur = cur.left;
+            } else {
+                return cur;
+            }
+        }
+        return null;
+    }
+}
