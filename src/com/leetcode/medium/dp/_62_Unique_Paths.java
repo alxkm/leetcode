27be8a6efbdb0cae01c03f1472c092a65c18b2d1
Oleksandr Klymenko <okly@telenor.dk>
2023-07-15 22:38:08 +0300

    Update links

--- src/com/leetcode/medium/dp/_62_Unique_Paths.java
+++ src/com/leetcode/medium/dp/_62_Unique_Paths.java
@@ -1,6 +1,8 @@
 package com.leetcode.medium.dp;
 
 public class _62_Unique_Paths {
+    //https://leetcode.com/problems/unique-paths/
+
     static public int uniquePaths(int m, int n) {
 
         int[][] a = new int[m][n];
@@ -18,6 +20,22 @@ public class _62_Unique_Paths {
         }
 
         return a[m - 1][n - 1];
+    }
+
+    public int uniquePaths1(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]);
+                }
+
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
     public static void main(String[] args) {
--- src/com/leetcode/medium/dp/_63_Unique_Paths_II.java
+++ src/com/leetcode/medium/dp/_63_Unique_Paths_II.java
@@ -1,6 +1,8 @@
 package com.leetcode.medium.dp;
 
 public class _63_Unique_Paths_II {
+    //https://leetcode.com/problems/unique-paths-ii/
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
@@ -30,6 +32,27 @@ public class _63_Unique_Paths_II {
                 if (obstacleGrid[i][j] == 1) {
                     dp[i][j] = 0;
                 } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public int uniquePathsWithObstacles1(int[][] obstacleGrid) {
+        int[][] og = obstacleGrid;
+        int m = og.length, n = og[0].length;
+        int[][] dp = new int[m][n];
+        if (og == null || og[0][0] == 1 || og[m - 1][n - 1] == 1) return 0;
+        dp[0][0] = 1;
+        for (int i = 1, j = 1; i < m || j < n; i++, j++) {
+            if (i < m) dp[i][0] = (og[i][0] == 1) ? 0 : dp[i - 1][0];
+            if (j < n) dp[0][j] = (og[0][j] == 1) ? 0 : dp[0][j - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (og[i][j] == 0) {
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                 }
             }
