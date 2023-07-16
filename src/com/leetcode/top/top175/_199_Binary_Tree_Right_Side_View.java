1e5a0e63462a7cc2214a67e3b5f2ed484b77be6d
Alex Klm <alexanderklmn@gmail.com>
2023-07-16 20:03:02 +0300

    https://leetcode.com/problems/binary-tree-right-side-view/

---
+++ src/com/leetcode/top/top175/_199_Binary_Tree_Right_Side_View.java
@@ -0,0 +1,27 @@
+package com.leetcode.top.top175;
+
+
+import com.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _199_Binary_Tree_Right_Side_View {
+    //https://leetcode.com/problems/binary-tree-right-side-view/
+    //todo todo todo
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> l = new ArrayList<>();
+        dfs(root, 0, l);
+        return l;
+    }
+
+    private void dfs(TreeNode t, int level, List<Integer> l) {
+        if (t == null) return;
+        if (level == l.size()) {
+            l.add(t.val);
+        }
+        dfs(t.right, level + 1, l);
+        dfs(t.left, level + 1, l);
+    }
+}
