8c490e79d6c7b9a2fbdf799b92a57f853467c96a
Oleksandr Klymenko <okly@telenor.dk>
2023-07-25 15:03:04 +0300

    Fix TreeNode

--- src/com/leetcode/TreeNode.java
+++ src/com/leetcode/TreeNode.java
@@ -5,6 +5,7 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
+    public TreeNode() { }
     public TreeNode(int val) { this.val = val; }
 
     public TreeNode(int val, TreeNode left, TreeNode right) {
