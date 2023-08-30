7b924faa724b203185246ef9495a3ada92a37d98
Oleksandr Klymenko <okly@telenor.dk>
2023-08-30 12:30:31 +0300

    https://leetcode.com/problems/koko-eating-bananas/

---
+++ src/com/leetcode/medium/binsearch/_875_Koko_Eating_Bananas.java
@@ -0,0 +1,30 @@
+package com.leetcode.medium.binsearch;
+
+import java.util.Arrays;
+
+public class _875_Koko_Eating_Bananas {
+    //https://leetcode.com/problems/koko-eating-bananas/
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = Arrays.stream(piles).max().getAsInt();
+        int min = r;
+
+        while (l < r + 1) {
+            int k = (l + r) / 2;
+            int hours = 0;
+            for (int n: piles) {
+                hours += Math.ceil((double) n/k);
+            }
+
+            if (hours <= h) {
+                min = Math.min(min, k);
+                r = k - 1;
+            } else {
+                l = k + 1;
+            }
+        }
+
+        return min;
+    }
+}
