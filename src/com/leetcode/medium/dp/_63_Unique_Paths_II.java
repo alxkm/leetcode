f89acd636f68d070020dbb5ace798cd30acdebf9
Oleksandr Klymenko <okly@telenor.dk>
2023-09-05 22:28:12 +0300

    https://leetcode.com/problems/unique-paths-ii/

--- src/com/leetcode/medium/dp/_63_Unique_Paths_II.java
+++ src/com/leetcode/medium/dp/_63_Unique_Paths_II.java
@@ -59,4 +59,35 @@ public class _63_Unique_Paths_II {
         }
         return dp[m - 1][n - 1];
     }
+
+    public int uniquePathsWithObstacles2(int[][] obs) {
+        if (obs[0][0] == 1) return 0;
+        int m = obs.length;
+        int n = obs[0].length;
+        if (obs[m - 1][n - 1] == 1) return 0;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) if (obs[i][0] == 1) dp[i][0] = 0; else dp[i][0] = dp[i - 1][0];
+        for (int j = 1; j < n; j++) if (obs[0][j] == 1) dp[0][j] = 0; else dp[0][j] = dp[0][j - 1];
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obs[i][j] == 1) dp[i][j] = 0;
+                else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] a = new int[][]{
+                {0, 0, 0},
+                {0, 1, 0},
+                {0, 0, 0}
+        };
+        var sol = new _63_Unique_Paths_II();
+        System.out.println(sol.uniquePathsWithObstacles2(a));
+
+    }
 }
