f6c322abffef0f1a52404ee0df8b416ae122dbe3
Alex Klm <alexanderklmn@gmail.com>
2023-09-08 18:06:13 +0200

    https://leetcode.com/problems/combination-sum-iii/

---
+++ src/com/leetcode/medium/backtrack/_216_Combination_Sum_III.java
@@ -0,0 +1,42 @@
+package com.leetcode.medium.backtrack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class _216_Combination_Sum_III {
+    //https://leetcode.com/problems/combination-sum-iii/
+
+    private int k;
+    private int n;
+    private List<List<Integer>> ll = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        if (k > n) return ll;
+        this.k = k;
+        this.n = n;
+        f(1, 0, new Stack<>());
+        return ll;
+    }
+
+    private void f(int start, int s, Stack<Integer> st) {
+        if (st.size() == k && s == n) {
+            ll.add(new ArrayList<>(st));
+            return;
+        }
+
+        if ((s + start) >  n) return;
+        if (st.size() >= k) return;
+
+        for (int i = start; i <= 9; i++) {
+            st.add(i);
+            f(i + 1, s + i, st);
+            st.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        var sol = new _216_Combination_Sum_III();
+        System.out.println(sol.combinationSum3(2, 18));
+    }
+}
