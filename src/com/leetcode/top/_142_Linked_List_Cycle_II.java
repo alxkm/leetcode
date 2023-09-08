7d656c359ff23503090a08b2dc5e29b85040a746
Oleksandr Klymenko <okly@telenor.dk>
2023-09-08 19:06:48 +0300

    https://leetcode.com/problems/linked-list-cycle-ii/

---
+++ src/com/leetcode/top/_142_Linked_List_Cycle_II.java
@@ -0,0 +1,38 @@
+package com.leetcode.top;
+
+import com.leetcode.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class _142_Linked_List_Cycle_II {
+    //https://leetcode.com/problems/linked-list-cycle-ii/
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                ListNode slow2 = head;
+                while (slow2 != slow) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow;
+            }
+        }
+        //System.out.println(slow.val);
+        return null;
+    }
+}
