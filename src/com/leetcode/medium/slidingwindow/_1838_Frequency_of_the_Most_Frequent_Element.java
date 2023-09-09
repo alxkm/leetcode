6b004cb5500f296c5f6a2f837294891aafd3c602
Alex Klm <alexanderklmn@gmail.com>
2023-09-09 21:17:03 +0200

    https://leetcode.com/problems/frequency-of-the-most-frequent-element/

---
+++ src/com/leetcode/medium/slidingwindow/_1838_Frequency_of_the_Most_Frequent_Element.java
@@ -0,0 +1,25 @@
+package com.leetcode.medium.slidingwindow;
+
+import java.util.Arrays;
+
+public class _1838_Frequency_of_the_Most_Frequent_Element {
+    //https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+    //FOREIGN_SOLUTION
+    //TOP
+
+    public int maxFrequency(int[] nums, int k) {
+        int max = 1;
+        Arrays.sort(nums);
+        long totalSum = 0;
+
+        for (int l = 0, r = 0; r < nums.length; r++) {
+            totalSum += nums[r];
+            while ((long)nums[r]*(r - l + 1) > totalSum + k) {
+                totalSum -= nums[l];
+                l++;
+            }
+            max = Math.max(max, r - l + 1);
+        }
+        return max;
+    }
+}
