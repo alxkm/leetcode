ec77ae595faae411567b85f5e3f97eec733cf1da
Oleksandr Klymenko <okly@telenor.dk>
2023-09-22 13:56:23 +0300

    https://leetcode.com/problems/decode-ways/description/

---
+++ src/com/leetcode/top/top75/_91_Decode_Ways.java
@@ -0,0 +1,26 @@
+package com.leetcode.top.top75;
+
+public class _91_Decode_Ways {
+    //https://leetcode.com/problems/decode-ways/description/
+    //FOREIGN_SOLUTION
+    //TOP
+
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(0) == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+            }
+            if (i + 1 < n && (s.charAt(i) == '1' || s.charAt(i) == '0' && s.charAt(i + 1) < 7)) {
+                dp[i] += dp[i + 2];
+            }
+        }
+
+        return dp[0];
+    }
+}
