dfcdc1a270a2c0cd1f7e6da5deec95e4ec3700ba
Oleksandr Klymenko <okly@telenor.dk>
2023-09-22 13:50:41 +0300

    https://leetcode.com/problems/deepest-leaves-sum/

---
+++ src/com/leetcode/medium/tree/_1302_Deepest_Leaves_Sum.java
@@ -0,0 +1,41 @@
+package com.leetcode.medium.tree;
+
+import com.leetcode.TreeNode;
+
+public class _1302_Deepest_Leaves_Sum {
+    //https://leetcode.com/problems/deepest-leaves-sum/
+    //TOP
+
+    private int n = 0;
+    private int sum = 0;
+
+    private void findMaxLengthDfs(TreeNode t, int size) {
+        if (t == null) {
+            return;
+        }
+        if (t.left == null && t.right == null) {
+            n = Math.max(size, n);
+            return;
+        }
+        findMaxLengthDfs(t.right, size + 1);
+        findMaxLengthDfs(t.left, size + 1);
+    }
+
+    private void sumDfs(TreeNode t, int size) {
+        if (t == null) {
+            return;
+        }
+        if (t.left == null && t.right == null && size == n) {
+            sum += t.val;
+            return;
+        }
+        sumDfs(t.right, size + 1);
+        sumDfs(t.left, size + 1);
+    }
+
+    public int deepestLeavesSum(TreeNode root) {
+        findMaxLengthDfs(root, 1);
+        sumDfs(root, 1);
+        return sum;
+    }
+}
