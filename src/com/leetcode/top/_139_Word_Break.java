6ba576c4049b39e7ce20803b88787333ebfb1324
Oleksandr Klymenko <okly@telenor.dk>
2023-09-22 14:34:41 +0300

    https://leetcode.com/problems/word-break/

---
+++ src/com/leetcode/top/_139_Word_Break.java
@@ -0,0 +1,30 @@
+package com.leetcode.top;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class _139_Word_Break {
+    //https://leetcode.com/problems/word-break/
+    //FOREIGN_SOLUTION
+    //DP
+    //TOP
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        Set<String> set = new HashSet<>(wordDict);
+        for (int i = 0; i < n; i++) {
+            if (!dp[i]) continue;
+
+            for (int j = i + 1; j <= n; j++) {
+                var str = s.substring(i, j);
+                if (set.contains(str)) {
+                    dp[j] = true;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
