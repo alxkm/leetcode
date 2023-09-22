58303feb84d36a9a0c0779d6fc1af5013c925383
Alex Klm <alexanderklmn@gmail.com>
2023-09-22 13:33:05 +0200

    https://leetcode.com/problems/sum-root-to-leaf-numbers/

---
+++ src/com/leetcode/medium/tree/_129_Sum_Root_to_Leaf_Numbers.java
@@ -0,0 +1,19 @@
+package com.leetcode.medium.tree;
+
+import com.leetcode.TreeNode;
+
+public class _129_Sum_Root_to_Leaf_Numbers {
+    //https://leetcode.com/problems/sum-root-to-leaf-numbers/
+    //TREE
+    //TOP
+
+    public int sumNumbers(TreeNode root) {
+       return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode t, int sum) {
+        if (t == null) return 0;
+        if (t.left == null && t.right == null) return t.val + sum * 10;
+        return dfs(t.left, t.val + sum * 10) + dfs(t.right, t.val + sum * 10);
+    }
+}
