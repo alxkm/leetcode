27cfe39215088246f16a4c69360bb828d080c2d0
alx <alx@alx.com>
2023-10-08 00:38:21 +0300

    https://leetcode.com/problems/delete-node-in-a-bst/

---
+++ src/com/leetcode/top/tree/_450_Delete_Node_in_a_BST.java
@@ -0,0 +1,51 @@
+package com.leetcode.top.tree;
+
+public class _450_Delete_Node_in_a_BST {
+    //https://leetcode.com/problems/delete-node-in-a-bst/
+    //TOP
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return root;
+        }
+
+        if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            var cur = root.right;
+            while (cur.left != null) {
+                cur = cur.left;
+            }
+            root.val = cur.val;
+            root.right = deleteNode(root.right, root.val);
+        }
+
+        return root;
+    }
+}
