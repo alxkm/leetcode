c1928e19225761d880adb23166cee7bc0095dda6
alx <alx@alx.com>
2023-10-08 00:37:54 +0300

    https://leetcode.com/problems/maximum-binary-tree/

---
+++ src/com/leetcode/top/top175/_654_Maximum_Binary_Tree.java
@@ -0,0 +1,29 @@
+package com.leetcode.top.top175;
+
+import com.leetcode.TreeNode;
+
+import java.util.Stack;
+
+public class _654_Maximum_Binary_Tree {
+    //https://leetcode.com/problems/maximum-binary-tree/
+    //FOREIGN_SOLUTION
+    //TOP
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        TreeNode root = new TreeNode();
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        for (int n: nums) {
+            TreeNode node = new TreeNode(n);
+            while (!stack.isEmpty() && stack.peek().val < n) {
+                node.left = stack.pop();
+            }
+            if (!stack.isEmpty() && stack.peek().val > n) {
+                stack.peek().right = node;
+            }
+            stack.push(node);
+        }
+        return stack.get(0);
+    }
+}
