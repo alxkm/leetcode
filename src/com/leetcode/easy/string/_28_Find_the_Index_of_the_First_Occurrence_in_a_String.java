02677ba256f21598c67124f2ef69ec6ac80c8d13
alx <alx@alx.com>
2023-10-12 23:50:26 +0300

    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/

--- src/com/leetcode/easy/string/_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
+++ src/com/leetcode/easy/string/_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -1,8 +1,8 @@
 package com.leetcode.easy.string;
 
 public class _28_Find_the_Index_of_the_First_Occurrence_in_a_String {
-    //https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
     //28. Find the Index of the First Occurrence in a String
+    //https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
     public int strStr(String haystack, String needle) {
         if (haystack == null || haystack.equals("")) return -1;
