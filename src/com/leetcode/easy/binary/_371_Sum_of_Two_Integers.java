93a1b9ff7b17fdd51c1f00d45a888fe66468c13d
Alex Klm <alexanderklmn@gmail.com>
2023-10-21 22:25:53 +0200

    https://leetcode.com/problems/sum-of-two-integers/

---
+++ src/com/leetcode/easy/binary/_371_Sum_of_Two_Integers.java
@@ -0,0 +1,19 @@
+package com.leetcode.easy.binary;
+
+public class _371_Sum_of_Two_Integers {
+    //https://leetcode.com/problems/sum-of-two-integers/
+    //371. Sum of Two Integers
+    //FOREIGN_SOLUTION
+
+    public int getSum(int a, int b) {
+        if(a==0 || b==0) return a!=0?a:b;
+        int c=b,s=0;
+        while(c!=0){
+            s=a^b;
+            c=(a&b)<<1;
+            a=s;
+            b=c;
+        }
+        return s;
+    }
+}
