940ae03607ef03048c82f2af4fe960adf86ce493
alx <alx@alx.com>
2023-10-21 23:05:47 +0300

    https://leetcode.com/problems/fair-candy-swap/

---
+++ src/com/leetcode/easy/top/_888_Fair_Candy_Swap.java
@@ -0,0 +1,31 @@
+package com.leetcode.easy.top;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _888_Fair_Candy_Swap {
+    //https://leetcode.com/problems/fair-candy-swap/
+    //888. Fair Candy Swap
+
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int sumA = 0, sumB = 0;
+        for (int a: aliceSizes) {
+            sumA += a;
+        }
+
+        Set<Integer> setB = new HashSet<>();
+        for (int b: bobSizes) {
+            sumB += b;
+            setB.add(b);
+        }
+        int[] answer = new int[2];
+        int delta = (sumB - sumA) / 2;
+        for (int a: aliceSizes) {
+            if (setB.contains(a + delta)) {
+                return new int[]{a, a + delta};
+            }
+        }
+
+        return null;
+    }
+}
