b8c8bcf3b64c59c6e75caa90413628f557195e5f
Alex Klm <alexanderklmn@gmail.com>
2023-10-21 22:21:23 +0200

    https://leetcode.com/problems/removing-stars-from-a-string/

---
+++ src/com/leetcode/easy/hash/_217_Contains_Duplicate.java
@@ -0,0 +1,18 @@
+package com.leetcode.easy.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _217_Contains_Duplicate {
+    //https://leetcode.com/problems/contains-duplicate/
+    //217. Contains Duplicate
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n: nums) {
+            if (set.contains(n)) return true;
+            set.add(n);
+        }
+        return false;
+    }
+}
