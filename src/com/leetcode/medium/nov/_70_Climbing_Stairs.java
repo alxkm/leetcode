3a7aed51b8331e3cbb6fcb31c71d64ae40e82d90
alx <alx@alx.com>
2023-11-21 18:33:50 +0200

    https://leetcode.com/problems/climbing-stairs/

---
+++ src/com/leetcode/medium/nov/_70_Climbing_Stairs.java
@@ -0,0 +1,18 @@
+package com.leetcode.medium.nov;
+
+public class _70_Climbing_Stairs {
+    public int climbStairs(int n) {
+        //https://leetcode.com/problems/climbing-stairs/
+
+        int dp = 0;
+        int f = 1, s = 1;
+        if (n == 1) return 1;
+        for (int i = 1; i < n; i++) {
+            dp = f + s;
+            int temp = f;
+            f = s;
+            s = temp + f;
+        }
+        return dp;
+    }
+}
