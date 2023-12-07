bbeff275c1f0da0a7feedbf3ee525aa2a7e2106c
alx <alx@alx.com>
2023-12-07 11:04:25 +0200

    https://leetcode.com/problems/largest-odd-number-in-string/

--- src/com/leetcode/easy/string/additional/_1903_Largest_Odd_Number_in_String.java
+++ src/com/leetcode/easy/string/additional/_1903_Largest_Odd_Number_in_String.java
@@ -1,10 +1,30 @@
 package com.leetcode.easy.string.additional;
 
 public class _1903_Largest_Odd_Number_in_String {
+    //https://leetcode.com/problems/largest-odd-number-in-string/
+    //1903. Largest Odd Number in String
+
     static public String largestOddNumber(String num) {
         for (int i = num.length() - 1; i >= 0; i--) {
             int temp = (int) num.charAt(i) - 48;
             if (temp % 2 == 0) continue;
+            return num.substring(0, i + 1);
+        }
+        return "";
+    }
+
+    public String largestOddNumber1(String num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int n = (num.charAt(i) - '0');
+            if (n % 2 != 0 || sb.length() != 0) sb.insert(0, n);
+        }
+        return sb.toString();
+    }
+
+    public String largestOddNumber2(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if ((num.charAt(i) - '0') % 2 == 0) continue;
             return num.substring(0, i + 1);
         }
         return "";
