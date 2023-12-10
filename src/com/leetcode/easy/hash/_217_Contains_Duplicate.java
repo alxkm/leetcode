e4857643da17a0f17d35b06859922ae744da3c86
alx <alx@alx.com>
2023-12-11 00:51:34 +0200

    https://leetcode.com/problems/contains-duplicate/

--- src/com/leetcode/easy/hash/_217_Contains_Duplicate.java
+++ src/com/leetcode/easy/hash/_217_Contains_Duplicate.java
@@ -9,10 +9,31 @@ public class _217_Contains_Duplicate {
 
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for (int n: nums) {
+        for (int n : nums) {
             if (set.contains(n)) return true;
             set.add(n);
         }
         return false;
+    }
+
+    public boolean containsDuplicate1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            int j = i - 1;
+            while (j >= 0 && current < nums[j]) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            if (j >= 0 && nums[j] == current) {
+                return true;
+            }
+            nums[j + 1] = current;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        var sol = new _217_Contains_Duplicate();
+        System.out.println(sol.containsDuplicate1(new int[]{1, 2, 3, 1}));
     }
 }
