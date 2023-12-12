9088d76f2907a37a94b0b7e3257d8d821887830a
alx <alx@alx.com>
2023-12-12 16:16:57 +0200

    https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/

--- src/com/leetcode/easy/sort/_1464_Maximum_Product_of_Two_Elements_in_an_Array.java
+++ src/com/leetcode/easy/sort/_1464_Maximum_Product_of_Two_Elements_in_an_Array.java
@@ -3,6 +3,9 @@ package com.leetcode.easy.sort;
 import java.util.Arrays;
 
 public class _1464_Maximum_Product_of_Two_Elements_in_an_Array {
+    //1464. Maximum Product of Two Elements in an Array
+    //https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+
     public int maxProduct(int[] nums) {
         //int product = 0;
         // for (int i = 0; i < nums.length; i++) {
@@ -12,5 +15,10 @@ public class _1464_Maximum_Product_of_Two_Elements_in_an_Array {
         // }
         Arrays.sort(nums);
         return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1) ;
+    }
+
+    public int maxProduct1(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
     }
 }
