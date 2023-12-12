4162fcee50877c0e3d13a0db8de5464bde8f0119
alx <alx@alx.com>
2023-12-12 16:15:20 +0200

    https://leetcode.com/problems/calculate-money-in-leetcode-bank/

--- src/com/leetcode/easy/math/_1716_Calculate_Money_in_Leetcode_Bank.java
+++ src/com/leetcode/easy/math/_1716_Calculate_Money_in_Leetcode_Bank.java
@@ -1,6 +1,7 @@
 package com.leetcode.easy.math;
 
 public class _1716_Calculate_Money_in_Leetcode_Bank {
+    //1716. Calculate Money in Leetcode Bank
     //https://leetcode.com/problems/calculate-money-in-leetcode-bank/
 
     public int totalMoney(int n) {
@@ -19,5 +20,17 @@ public class _1716_Calculate_Money_in_Leetcode_Bank {
             n--;
         }
         return sum;
+    }
+
+    public int totalMoney1(int n) {
+        int s = 0;
+        for (int i = 0, start = 1; n > 0; n--) {
+            s += start + i;
+            if (++i == 7) {
+                i = 0;
+                start++;
+            }
+        }
+        return s;
     }
 }
