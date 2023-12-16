2c229d2b5392a852f6d511499119a126d53829e9
alx <alx@alx.com>
2023-12-17 00:34:44 +0200

    //https://leetcode.com/problems/climbing-stairs/

--- src/com/leetcode/easy/dp/_70_Climbing_Stairs.java
+++ src/com/leetcode/easy/dp/_70_Climbing_Stairs.java
@@ -1,6 +1,9 @@
 package com.leetcode.easy.dp;
 
 public class _70_Climbing_Stairs {
+    //70. Climbing Stairs
+    //https://leetcode.com/problems/climbing-stairs/
+
     public int climbStairs(int n) {
         int dp = 0;
         int f = 1, s = 1;
@@ -12,5 +15,20 @@ public class _70_Climbing_Stairs {
             s = temp + f;
         }
         return dp;
+    }
+
+    public int climbStairs1(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <=n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 }
