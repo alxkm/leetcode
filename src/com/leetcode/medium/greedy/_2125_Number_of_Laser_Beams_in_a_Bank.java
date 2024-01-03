ee824db6fa4b3eabf1c43288ee8d9b63dad85d98
alx <alx@alx.com>
2024-01-03 11:19:14 +0200

    https://leetcode.com/problems/number-of-laser-beams-in-a-bank/

---
+++ src/com/leetcode/medium/greedy/_2125_Number_of_Laser_Beams_in_a_Bank.java
@@ -0,0 +1,29 @@
+package com.leetcode.medium.greedy;
+
+public class _2125_Number_of_Laser_Beams_in_a_Bank {
+    //https://leetcode.com/problems/number-of-laser-beams-in-a-bank/
+    //2125. Number of Laser Beams in a Bank
+
+    public int numberOfBeams(String[] bank) {
+        int prev = count(bank[0]);
+        int cnt = 0;
+
+        for (int i = 1; i < bank.length; i++) {
+            int cur = count(bank[i]);
+            if (cur == 0) {
+                continue;
+            }
+            cnt += cur * prev;
+            prev = cur;
+        }
+        return cnt;
+    }
+
+    private int count(String s) {
+        int cnt = 0;
+        for (char ch: s.toCharArray()) {
+            cnt += ch - '0';
+        }
+        return cnt;
+    }
+}
