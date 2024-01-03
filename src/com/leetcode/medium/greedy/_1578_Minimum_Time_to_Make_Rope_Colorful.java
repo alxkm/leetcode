f7bb7018111324ea6ef03b1f2da1a90501348571
alx <alx@alx.com>
2024-01-03 11:22:31 +0200

    https://leetcode.com/problems/minimum-time-to-make-rope-colorful/

---
+++ src/com/leetcode/medium/greedy/_1578_Minimum_Time_to_Make_Rope_Colorful.java
@@ -0,0 +1,29 @@
+package com.leetcode.medium.greedy;
+
+public class _1578_Minimum_Time_to_Make_Rope_Colorful {
+    //1578. Minimum Time to Make Rope Colorful
+    //https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
+
+    public int minCost(String colors, int[] neededTime) {
+        int time = 0;
+        char[] ca = colors.toCharArray();
+        char ch = ca[0];
+        int max = neededTime[0];
+
+        for (int i = 1; i < ca.length; i++) {
+            if (ca[i] == ch) {
+                if (max <= neededTime[i]) {
+                    time += max;
+                    max = neededTime[i];
+                } else {
+                    time += neededTime[i];
+                }
+            } else {
+                ch = ca[i];
+                max = neededTime[i];
+            }
+        }
+
+        return time;
+    }
+}
