e7e4759db9db866fcf6cab66216a02d215189228
Alex Klm <alexanderklmn@gmail.com>
2024-01-03 11:18:04 +0200

    //https://leetcode.com/problems/image-smoother/

---
+++ src/com/leetcode/easy/matrix/_661_Image_Smoother.java
@@ -0,0 +1,36 @@
+package com.leetcode.easy.matrix;
+
+public class _661_Image_Smoother {
+    //661. Image Smoother
+    //https://leetcode.com/problems/image-smoother/
+
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length, n = img[0].length;
+        int[][] ar = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int sum = 0;
+                int count = 0;
+                for (int k = i - 1; k <= i + 1; k++) {
+                    for (int l = j - 1; l <= j + 1; l++) {
+                        if (k >= 0 && k < m && l >= 0 && l < n) {
+                            sum += img[k][l];
+                            count++;
+                        }
+                    }
+                }
+                ar[i][j] = sum / count;
+            }
+        }
+
+        return ar;
+    }
+
+    public static void main(String[] args) {
+        int[][] img = {{100, 200, 100}, {200, 50, 200}, {100, 200, 100}};
+        var sol = new _661_Image_Smoother();
+        sol.imageSmoother(img);
+    }
+}
