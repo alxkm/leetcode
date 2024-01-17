e032bad31fea617baf3aee2ed1ef9ba21b54e6a1
alx <alx@alx.com>
2024-01-17 17:08:42 +0200

    https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/

--- src/com/leetcode/easy/array/_1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.java
+++ src/com/leetcode/easy/array/_1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.java
@@ -2,6 +2,8 @@ package com.leetcode.easy.array;
 
 public class _1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary {
     //https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+    //1491. Average Salary Excluding the Minimum and Maximum Salary
+    //top
 
     public double average(int[] salary) {
         int max = Integer.MIN_VALUE;
