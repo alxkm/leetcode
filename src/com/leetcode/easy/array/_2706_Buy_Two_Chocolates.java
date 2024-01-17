0dcb56f3128a0c4ded0e5f6fe1cf18665e4cf675
alx <alx@alx.com>
2024-01-17 17:09:12 +0200

    https://leetcode.com/problems/buy-two-chocolates/

---
+++ src/com/leetcode/easy/array/_2706_Buy_Two_Chocolates.java
@@ -0,0 +1,27 @@
+package com.leetcode.easy.array;
+
+public class _2706_Buy_Two_Chocolates {
+    //2706. Buy Two Chocolates
+    //https://leetcode.com/problems/buy-two-chocolates/
+
+    public int buyChoco(int[] prices, int money) {
+        int min1 = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            min1 = Math.min(min1, prices[i]);
+        }
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] == min1) {
+                prices[i] = Integer.MAX_VALUE;
+                break;
+            }
+        }
+        int min2 = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            min2 = Math.min(min2, prices[i]);
+        }
+        if (min1 + min2 > money) {
+            return money;
+        }
+        return money - min1 - min2;
+    }
+}
