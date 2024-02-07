71050a06d159bcf93c428cee9db3ef7ecd11df3a
Alex Klm <alexanderklmn@gmail.com>
2024-02-07 14:02:48 +0200

    https://leetcode.com/problems/sort-characters-by-frequency/

--- src/com/leetcode/top/top175/_451_Sort_Characters_By_Frequency.java
+++ src/com/leetcode/top/top175/_451_Sort_Characters_By_Frequency.java
@@ -1,5 +1,7 @@
 package com.leetcode.top.top175;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,5 +30,29 @@ public class _451_Sort_Characters_By_Frequency {
             }
         }
         return sb.toString();
+    }
+
+    public String frequencySort1(String s) {
+        int[][] fr = new int[75][2];
+        for (char ch: s.toCharArray())  {
+            int i = ch - '0';
+            fr[i][0] = i;
+            fr[i][1]++;
+        }
+        Arrays.sort(fr, (x, y) -> y[1] - x[1]);
+        StringBuilder sb = new StringBuilder();
+        for (int[] x: fr) {
+            if (x[1] != 0) {
+                for (int i = 0; i < x[1]; i++) {
+                    sb.append((char)(x[0] + '0'));
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        var sol = new _451_Sort_Characters_By_Frequency();
+        System.out.println(sol.frequencySort1("tree"));
     }
 }
