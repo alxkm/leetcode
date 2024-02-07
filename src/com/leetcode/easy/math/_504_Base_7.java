bd31bc7928e4bc3ff59cb137d4d82d4258541424
Alex Klm <alexanderklmn@gmail.com>
2024-02-07 14:03:52 +0200

    https://leetcode.com/problems/base-7/

---
+++ src/com/leetcode/easy/math/_504_Base_7.java
@@ -0,0 +1,24 @@
+package com.leetcode.easy.math;
+
+public class _504_Base_7 {
+    //504. Base 7
+    //https://leetcode.com/problems/base-7/
+
+    public String convertToBase7(int num) {
+        if (num == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        boolean isNegative = num < 0;
+        if (isNegative) {
+            num *= -1;
+        }
+        while (num > 0) {
+            sb.append(num % 7);
+            num /= 7;
+        }
+        sb.reverse();
+        if (isNegative) {
+            sb.insert(0, '-');
+        }
+        return sb.toString();
+    }
+}
