00b2d931e369cf07225915f9d6ddb4003bfac15a
alx <alx@alx.com>
2024-02-14 19:17:20 +0200

    https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/

---
+++ src/com/leetcode/easy/string/_1662_Check_If_Two_String_Arrays_are_Equivalent.java
@@ -0,0 +1,14 @@
+package com.leetcode.easy.string;
+
+public class _1662_Check_If_Two_String_Arrays_are_Equivalent {
+    //1662. Check If Two String Arrays are Equivalent
+    //https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        for (String s: word1) sb1.append(s);
+        StringBuilder sb2 = new StringBuilder();
+        for (String s: word2) sb2.append(s);
+        return sb1.toString().equals(sb2.toString());
+    }
+}
