7db2fa16cbf839c26152b912dd2244e1d7ad16e1
alx <alx@alx.com>
2024-02-20 15:43:03 +0200

    https://leetcode.com/problems/number-of-1-bits/

---
+++ src/com/leetcode/easy/top/array/_191_Number_of_1_Bits.java
@@ -0,0 +1,36 @@
+package com.leetcode.easy.top.array;
+
+public class _191_Number_of_1_Bits {
+    // you need to treat n as an unsigned value
+    //191. Number of 1 Bits
+    //https://leetcode.com/problems/number-of-1-bits/
+
+    public int hammingWeight(int n) {
+        int cnt = 0;
+        String s = Integer.toBinaryString(n);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') cnt++;
+        }
+        return cnt;
+    }
+
+    public int hammingWeight1(int n) {
+        int q = 0;
+
+        if (n < 0) {
+            n = ~n;
+            while (n > 0) {
+                q += n % 2;
+                n /= 2;
+            }
+            return 32 - q;
+        }
+
+        while (n > 0) {
+            q += n % 2;
+            n /= 2;
+        }
+
+        return q;
+    }
+}
