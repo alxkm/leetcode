d1e9948a9193dad0e5d5cf10d236b20bf7c57592
alx <alx@alx.com>
2024-02-27 15:08:58 +0200

    https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/

--- src/com/leetcode/easy/top/_1005_Maximize_Sum_Of_Array_After_K_Negation.java
+++ src/com/leetcode/easy/top/_1005_Maximize_Sum_Of_Array_After_K_Negation.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class _1005_Maximize_Sum_Of_Array_After_K_Negation {
     //1005. Maximize Sum Of Array After K Negations
     //https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+    //easyTop
 
     public int largestSumAfterKNegations(int[] nums, int k) {
         Arrays.sort(nums);
@@ -71,5 +72,45 @@ public class _1005_Maximize_Sum_Of_Array_After_K_Negation {
             s += nums[i];
         }
         return s;
+    }
+
+    //foreign solution
+
+    public int largestSumAfterKNegations2(int[] nums, int k) {
+        int[] numbers = new int[201];
+        int sum = 0;
+        int maxAbs = 0;
+        for (int n: nums) {
+            maxAbs = Math.max(maxAbs, Math.abs(n));
+            numbers[100 + n]++;
+            sum += n;
+        }
+        if (maxAbs == 0) {
+            return 0;
+        }
+        while (k-- != 0) {
+            int i = 100 - maxAbs;
+            while (numbers[i] == 0) {
+                i++;
+            }
+            numbers[i]--;
+            numbers[200 - i]++;
+            sum -= 2 * (i - 100);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {4, 2, 3};
+        int k1 = 1;
+
+        int[] nums2 = {3, -1, 0, 2};
+        int k2 = 3;
+
+        int[] nums3 = {2, -3, -1, 5, -4};
+        int k3 = 2;
+
+        int[] nums4 = {-100, -1, 0, 2, 1, 2};
+        int k4 = 4;
     }
 }
