973f152250984e93d3e0e05c0cd356f7ad0bc345
alx <alx@alx.com>
2024-03-15 23:41:33 +0200

    https://leetcode.com/problems/merge-two-sorted-lists/

--- src/com/leetcode/easy/list/_21_Merge_Two_Sorted_Lists.java
+++ src/com/leetcode/easy/list/_21_Merge_Two_Sorted_Lists.java
@@ -3,6 +3,7 @@ package com.leetcode.easy.list;
 import com.leetcode.ListNode;
 
 public class _21_Merge_Two_Sorted_Lists {
+    //21. Merge Two Sorted Lists
     //https://leetcode.com/problems/merge-two-sorted-lists/
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
@@ -27,6 +28,46 @@ public class _21_Merge_Two_Sorted_Lists {
         }
         h.next = list1 != null ? list1 : list2;
         return head.next;
+    }
+
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        if (list2 == null && list1 != null) return list1;
+        if (list1 == null && list2 != null) return list2;
+
+        ListNode head = new ListNode();
+        ListNode newHead = head;
+
+        while (list1 != null || list2 != null) {
+            if (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    head.next = list1;
+                    list1 = list1.next;
+                } else if (list1.val > list2.val) {
+                    head.next = list2;
+                    list2 = list2.next;
+                } else {
+                    head.next = list1;
+                    list1 = list1.next;
+                    head = head.next;
+                    head.next = list2;
+                    list2 = list2.next;
+                }
+                head = head.next;
+            } else {
+                while (list1 != null) {
+                    head.next = list1;
+                    list1 = list1.next;
+                    head = head.next;
+                }
+                while (list2 != null) {
+                    head.next = list2;
+                    list2 = list2.next;
+                    head = head.next;
+                }
+            }
+        }
+
+        return newHead.next;
     }
 
 }
