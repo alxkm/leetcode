9c66d85d67cb7759e5c69440769937aab39801b0
alx <alx@alx.com>
2024-03-28 13:45:05 +0200

    https://leetcode.com/problems/maximum-depth-of-binary-tree/

--- src/com/leetcode/easy/tree/_104_Maximum_Depth_of_Binary_Tree.java
+++ src/com/leetcode/easy/tree/_104_Maximum_Depth_of_Binary_Tree.java
@@ -3,12 +3,19 @@ package com.leetcode.easy.tree;
 import com.leetcode.TreeNode;
 
 public class _104_Maximum_Depth_of_Binary_Tree {
+    //104. Maximum Depth of Binary Tree
+    //https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
     static public int maxDepth(TreeNode root) {
         if (root == null) return 0;
         int l = maxDepth(root.left);
         int r = maxDepth(root.right);
         return (l > r) ? l + 1 : r + 1;
+    }
+
+    public int maxDepth1(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth1(root.left), maxDepth1(root.right)) + 1;
     }
 
 
