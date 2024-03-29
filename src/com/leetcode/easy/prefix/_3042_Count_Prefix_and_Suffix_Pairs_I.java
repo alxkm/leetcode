5a2ddb266d6a433f85cd4d13318ddb9423e75d0d
alx <alx@alx.com>
2024-03-29 19:19:54 +0200

    https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/

---
+++ src/com/leetcode/easy/prefix/_3042_Count_Prefix_and_Suffix_Pairs_I.java
@@ -0,0 +1,18 @@
+package com.leetcode.easy.prefix;
+
+public class _3042_Count_Prefix_and_Suffix_Pairs_I {
+    //3042. Count Prefix and Suffix Pairs I
+    //https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/
+
+    public int countPrefixSuffixPairs(String[] words) {
+        int counter = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+}
