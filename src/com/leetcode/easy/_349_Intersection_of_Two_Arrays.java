665ec8286f4c532895bf6dfb27269b88d0a209ed
alx <alx@alx.com>
2024-03-29 19:27:21 +0200

    https://leetcode.com/problems/intersection-of-two-arrays/

--- src/com/leetcode/easy/_349_Intersection_of_Two_Arrays.java
+++ src/com/leetcode/easy/_349_Intersection_of_Two_Arrays.java
@@ -4,6 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class _349_Intersection_of_Two_Arrays {
+    //349. Intersection of Two Arrays
+    //https://leetcode.com/problems/intersection-of-two-arrays/
+
     class Solution {
         public int[] intersection(int[] nums1, int[] nums2) {
             Set<Integer> s = new HashSet<>();
@@ -24,6 +27,27 @@ public class _349_Intersection_of_Two_Arrays {
             }
             return a;
         }
+    }
+
+    public int[] intersection1(int[] nums1, int[] nums2) {
+        int[] counterNums2 = new int[1001];
+        for (int n: nums2) counterNums2[n]++;
+
+        boolean[] inter = new boolean[1001];
+        int size = 0;
+        for (int n: nums1) {
+            if (counterNums2[n] > 0 && !inter[n]) {
+                inter[n] = true;
+                size++;
+            }
+        }
+        int[] ar = new int[size];
+
+        for (int i = 0, j = 0; i < inter.length; i++) {
+            if (inter[i]) ar[j++] = i;
+        }
+
+        return ar;
     }
 
     public static void main(String[] args) {
