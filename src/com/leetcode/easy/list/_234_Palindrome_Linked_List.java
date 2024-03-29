24e191368a1234e87839481d982cce6f8e046bf3
alx <alx@alx.com>
2024-03-29 19:19:16 +0200

    https://leetcode.com/problems/palindrome-linked-list/

--- src/com/leetcode/easy/list/_234_Palindrome_Linked_List.java
+++ src/com/leetcode/easy/list/_234_Palindrome_Linked_List.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.Stack;
 
 public class _234_Palindrome_Linked_List {
+    //234. Palindrome Linked List
+    //https://leetcode.com/problems/palindrome-linked-list/
+
     static public boolean isPalindrome(ListNode head) {
         StringBuilder sb = new StringBuilder();
         while (head != null) {
@@ -55,7 +58,45 @@ public class _234_Palindrome_Linked_List {
         return true;
     }
 
+    public static boolean isPalindrome2(ListNode head) {
+        if (head == null) return true;
+        ListNode h = head;
+        int size = 0;
+        while (h != null) {
+            size++;
+            h = h.next;
+        }
+        if (size == 1) return true;
+
+        h = head;
+        for (int i = 0; i < size / 2 - 1; i++) h = h.next;
+        ListNode l1 = h;
+        ListNode l2 = h.next;
+        l1.next = null;
+
+        if (size % 2 != 0) l2 = l2.next;
+
+        h = head;
+        ListNode prev = null;
+
+        while (h != null) {
+            ListNode temp = h.next;
+            h.next = prev;
+            prev = h;
+            h = temp;
+        }
+        l1 = prev;
+
+        while (l1 != null) {
+            if (l1.val != l2.val) return false;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println(isPalindrome(ListUtil.build(List.of(1, 2, 2, 1))));
+        System.out.println(isPalindrome2(ListUtil.build(List.of(1, 2, 2, 1))));
     }
 }
