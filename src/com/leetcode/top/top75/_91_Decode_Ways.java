c063af549030396c43917800f66efeb01e888ef7
alx <alx@alx.com>
2024-04-07 19:45:52 +0300

    https://leetcode.com/problems/decode-ways/

--- src/com/leetcode/top/top75/_91_Decode_Ways.java
+++ src/com/leetcode/top/top75/_91_Decode_Ways.java
@@ -2,6 +2,7 @@ package com.leetcode.top.top75;
 
 public class _91_Decode_Ways {
     //https://leetcode.com/problems/decode-ways/
+    //91. Decode Ways
     //FOREIGN_SOLUTION
     //TOP
 
@@ -21,6 +22,24 @@ public class _91_Decode_Ways {
             }
         }
 
+        return dp[0];
+    }
+
+    public int numDecodings1(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) != '0') {
+                dp[i] = dp[i + 1];
+            } else {
+                dp[i] = 0;
+            }
+
+            if (i <= s.length() - 2 && ((s.charAt(i) == '1' && s.charAt(i + 1) <= '9') || (s.charAt(i) == '2' && s.charAt(i + 1) < '7'))) {
+                dp[i] += dp[i + 2];
+            }
+        }
         return dp[0];
     }
 }
