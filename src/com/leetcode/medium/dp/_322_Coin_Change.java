416c35831d0accc16e86cb27e73211fbc727d320
alx <alx@alx.com>
2024-04-07 20:14:33 +0300

    https://leetcode.com/problems/coin-change/

--- src/com/leetcode/medium/dp/_322_Coin_Change.java
+++ src/com/leetcode/medium/dp/_322_Coin_Change.java
@@ -1,10 +1,13 @@
 package com.leetcode.medium.dp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class _322_Coin_Change {
     //https://leetcode.com/problems/coin-change/
+    //322. Coin Change
+    //dp
 
     class Solution {
         public int coinChange(int[] coins, int amount) {
@@ -46,8 +49,42 @@ public class _322_Coin_Change {
         }
     }
 
+    public int coinChange1(int[] coins, int amount) {
+        //return f(coins, amount, new int[amount]);
+
+//        if (amount == 0) return 1;
+//        int[] dp = new int[amount + 1];
+//        dp[0] = 1;
+//        dp[1] = 1;
+//        for (int i = 2; i < amount; i++) {
+//            for (int j = 0; j < coins.length && (i - coins[j] >= 0); j++) {
+//                dp[i] += dp[i - coins[j]];
+//            }
+//        }
+//        System.out.println(dp[amount]);
+//        return dp[amount];
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 1;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length && (i - coins[j] >= 0); j++) {
+                if (i - coins[j] == 0) {
+                    dp[i] = 1;
+                    break;
+                }
+                dp[i] = Math.min(1 + dp[i - coins[j]], dp[i]);
+            }
+        }
+        System.out.println(dp[amount]);
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+
     public static void main(String[] args) {
         _322_Coin_Change solution = new _322_Coin_Change();
-        System.out.println(solution.coinChange(new int[] {1, 2, 5}, 11));
+        //System.out.println(solution.coinChange(new int[] {1, 2, 5}, 11));
+        //System.out.println(solution.coinChange1(new int[] {1, 2, 5}, 11));
+        //System.out.println(solution.coinChange1(new int[] {2}, 3));
+        //System.out.println(solution.coinChange1(new int[] {474,83,404,3}, 264));
     }
 }
