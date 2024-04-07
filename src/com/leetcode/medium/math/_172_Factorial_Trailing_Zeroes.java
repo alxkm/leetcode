3ecaf1a0933bd0f0f2d7129040d1a89df5477c16
alx <alx@alx.com>
2024-04-07 19:56:24 +0300

    https://leetcode.com/problems/factorial-trailing-zeroes/

--- src/com/leetcode/medium/math/_172_Factorial_Trailing_Zeroes.java
+++ src/com/leetcode/medium/math/_172_Factorial_Trailing_Zeroes.java
@@ -2,6 +2,7 @@ package com.leetcode.medium.math;
 
 public class _172_Factorial_Trailing_Zeroes {
     //https://leetcode.com/problems/factorial-trailing-zeroes/
+    //172. Factorial Trailing Zeroes
 
     public int trailingZeroes(int n) {
         if (n < 5) return 0;
