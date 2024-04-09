fc01ffdc9bb9747afa8a04be7268951a0ea0d8da
alx <alx@alx.com>
2024-04-09 20:31:53 +0300

    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/

--- src/com/leetcode/medium/tree/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
+++ src/com/leetcode/medium/tree/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -4,6 +4,7 @@ import com.leetcode.TreeNode;
 
 public class _235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
     //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+    //235. Lowest Common Ancestor of a Binary Search Tree
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         var cur = root;
