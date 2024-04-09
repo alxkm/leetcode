60c4978993cfad27bd531a3a0f2908368b5ffd8b
alx <alx@alx.com>
2024-04-09 20:18:32 +0300

    https://leetcode.com/problems/top-k-frequent-elements/

--- src/com/leetcode/top/top75/_347_Top_K_Frequent_Elements.java
+++ src/com/leetcode/top/top75/_347_Top_K_Frequent_Elements.java
@@ -5,9 +5,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class _347_Top_K_Frequent_Elements {
     //https://leetcode.com/problems/top-k-frequent-elements/
+    //347. Top K Frequent Elements
     //FOREIGN_SOLUTION
     //TOP
 
@@ -36,12 +39,22 @@ public class _347_Top_K_Frequent_Elements {
         return answer;
     }
 
+    public int[] topKFrequent1(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n: nums) map.put(n, map.getOrDefault(n, 0) + 1);
+        Queue<Integer> pq = new PriorityQueue<>((x, y) -> (map.get(y) - map.get(x)));
+        pq.addAll(map.keySet());
+        int[] ar = new int[k];
+        for (int i = 0; i < k; i++) {
+            ar[i] = pq.poll();
+        }
+        return ar;
+    }
+
     public static void main(String[] args) {
         int[] a = {1};
         int k = 1;
         _347_Top_K_Frequent_Elements solution = new _347_Top_K_Frequent_Elements();
         System.out.println(Arrays.toString(solution.topKFrequent(a, k)));
-
-        List<Integer>[] list = new List[10];
     }
 }
