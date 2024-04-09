458d6a7eb1821bfdd2e330869c5259f51e954241
alx <alx@alx.com>
2024-04-09 20:32:23 +0300

    https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/

--- src/com/leetcode/medium/math/_1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition.java
+++ src/com/leetcode/medium/math/_1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class _1498_Number_of_Subsequences_That_Satisfy_the_Given_Sum_Condition {
     //https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/
+    //1498. Number of Subsequences That Satisfy the Given Sum Condition
+    //two pointers
 
     public int numSubseq(int[] nums, int target) {
         Arrays.sort(nums);
