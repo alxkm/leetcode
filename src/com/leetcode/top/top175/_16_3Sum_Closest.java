b79371bb9babb12aadf73016d226fe9ac2224e4c
alx <alx@alx.com>
2024-04-09 20:29:51 +0300

    https://leetcode.com/problems/3sum-closest/

--- src/com/leetcode/top/top175/_16_3Sum_Closest.java
+++ src/com/leetcode/top/top175/_16_3Sum_Closest.java
@@ -1,7 +1,10 @@
 package com.leetcode.top.top175;
 
+import java.util.Arrays;
+
 public class _16_3Sum_Closest {
     //https://leetcode.com/problems/3sum-closest/
+    //16. 3Sum Closest
 
     public int threeSumClosest(int[] nums, int target) {
         int answer = 0;
@@ -17,5 +20,33 @@ public class _16_3Sum_Closest {
         }
 
         return answer;
+    }
+
+    public int threeSumClosest1(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = Integer.MAX_VALUE;
+        int curSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                curSum = nums[i] + nums[left] + nums[right];
+                if (curSum == target) return curSum;
+
+                if (Math.abs(curSum - target) < Math.abs(sum - target)) sum = curSum;
+
+                if (curSum <= target) {
+                    left++;
+                    while (nums[left] == nums[left - 1] && left < right) {
+                        left++;
+                    }
+                } else {
+                    right--;
+                }
+            }
+        }
+        return sum;
     }
 }
