1e6be02b980f095c83a009e8a9d527638580d937
alx <alx@alx.com>
2024-04-09 20:26:46 +0300

    https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/

--- src/com/leetcode/top/top175/greedy/_452_Minimum_Number_of_Arrows_to_Burst_Balloons.java
+++ src/com/leetcode/top/top175/greedy/_452_Minimum_Number_of_Arrows_to_Burst_Balloons.java
@@ -1,13 +1,14 @@
-package com.leetcode.top.top175;
+package com.leetcode.top.top175.greedy;
 
 import java.util.Arrays;
 
 public class _452_Minimum_Number_of_Arrows_to_Burst_Balloons {
     //https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+    //452. Minimum Number of Arrows to Burst Balloons
     //greedy
     //PART_SOLUTION
 
-    public int findMinArrowShots(int[][] points) {
+    public int findMinArrowShots1(int[][] points) {
         if (points.length == 0) return 0;
         Arrays.sort(points, (x, y) -> Integer.compare(x[1], y[1]));
         int counter = 1;
@@ -23,12 +24,29 @@ public class _452_Minimum_Number_of_Arrows_to_Burst_Balloons {
         return counter;
     }
 
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (x, y) -> y[0] - x[0]);
+        int counter = 1;
+        int start = points[0][1];
+        for (int i = 1; i < points.length; i++) {
+            int[] p = points[i];
+            if (start >= p[0]) continue;
+            else {
+                start = p[1];
+                counter++;
+            }
+        }
+        return counter;
+    }
+
     public static void main(String[] args) {
         var solution = new _452_Minimum_Number_of_Arrows_to_Burst_Balloons();
 
         int[][] ar = new int[][]{{10,16},{2,8},{1,6},{7,12}};
+        int[][] ar11 = new int[][]{{1,2},{3,4},{5,6},{7,8}};
         int[][] ar1 = new int[][]{{-2147483646,-2147483645},{2147483646,2147483647}};
         int[][] ar2 = new int[][]{{3,9},{7,12},{3,8},{6,8},{9,10},{2,9},{0,9},{3,9},{0,6},{2,8}};
-        solution.findMinArrowShots(ar2);
+        solution.findMinArrowShots(ar1);
+        System.out.println(solution.findMinArrowShots(ar11));
     }
 }
