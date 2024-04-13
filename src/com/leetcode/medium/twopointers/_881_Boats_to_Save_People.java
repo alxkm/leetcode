3f90af0c05ce869cbad6e7ff5fa9cf31629e88f6
Alex Klm <alexanderklmn@gmail.com>
2024-04-13 16:07:05 +0200

    https://leetcode.com/problems/boats-to-save-people/

---
+++ src/com/leetcode/medium/twopointers/_881_Boats_to_Save_People.java
@@ -0,0 +1,23 @@
+package com.leetcode.medium.twopointers;
+
+import java.util.Arrays;
+
+public class _881_Boats_to_Save_People {
+    //881. Boats to Save People
+    //https://leetcode.com/problems/boats-to-save-people/
+
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int l = 0;
+        int r = people.length - 1;
+        int boats = 0;
+        while (l <= r) {
+            if (people[r] + people[l] <= limit || l == r) {
+                l++;
+            }
+            r--;
+            boats++;
+        }
+        return boats;
+    }
+}
