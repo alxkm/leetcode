8e817da4869afd3cd62f38f42e6a6050b7ebfb49
alx <alx@alx.com>
2024-04-22 12:04:46 +0300

    https://leetcode.com/problems/island-perimeter/

--- src/com/leetcode/easy/graph/_463_Island_Perimeter.java
+++ src/com/leetcode/easy/graph/_463_Island_Perimeter.java
@@ -8,6 +8,9 @@ import java.util.Map;
 import java.util.Set;
 
 public class _463_Island_Perimeter {
+    //463. Island Perimeter
+    //https://leetcode.com/problems/island-perimeter/
+
     public int islandPerimeter(int[][] grid) {
         Map<Integer, Set<Integer>> v = new HashMap<>();
         List<Integer> a = new ArrayList<>();
@@ -74,5 +77,54 @@ public class _463_Island_Perimeter {
         }
 
         return counter;
+    }
+
+    public int islandPerimeter2(int[][] grid) {
+        boolean[] ar = new boolean[grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1 && !ar[i]) {
+                    ar[i] = true;
+                    //ar
+                }
+            }    
+        }
+    return 1;
+    }
+    public List<Integer> peopleIndexes(List<List<String>> favoriteCompanies) {
+        Map<String, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < favoriteCompanies.size(); i++) {
+            for (int j = 0; j < favoriteCompanies.get(i).size(); j++) {
+                map.computeIfAbsent(favoriteCompanies.get(i).get(j), k -> new ArrayList<>()).add(i);
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < favoriteCompanies.size(); i++) {
+            boolean needAdditionalCheck = true;
+            Set<Integer> set = new HashSet<>();
+            for (int j = 0; j < favoriteCompanies.get(i).size(); j++) {
+                if (map.get(favoriteCompanies.get(i).get(j)).size() == 1) {
+                    list.add(i);
+                    needAdditionalCheck = false;
+                    break;
+                } else {
+                    for (Integer integer : map.get(favoriteCompanies.get(i).get(j))) {
+                        if (integer != i) set.add(integer);
+                    }
+                }
+            }
+
+            if (needAdditionalCheck) {
+                boolean includesAll = false;
+                for (Integer n : set) {
+                    if (favoriteCompanies.get(n).containsAll(favoriteCompanies.get(i))) {
+                        includesAll = true;
+                        break;
+                    }
+                }
+                if (!includesAll) list.add(i);
+            }
+        }
+        return list;
     }
 }
