95aa5caa6998e210ec9a49044ce93f817cbcb7de
Alex Klm <alexanderklmn@gmail.com>
2024-04-24 11:51:08 +0200

    https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle/

---
+++ src/com/leetcode/easy/math/_3000_Maximum_Area_of_Longest_Diagonal_Rectangle.java
@@ -0,0 +1,19 @@
+package com.leetcode.easy.math;
+
+public class _3000_Maximum_Area_of_Longest_Diagonal_Rectangle {
+    //3000. Maximum Area of Longest Diagonal Rectangle
+    //https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle/
+
+    public int areaOfMaxDiagonal(int[][] dimensions) {
+        double prevDiagonal = 0;
+        int area = 0;
+        for (int[] d: dimensions) {
+            double curDiagonal = Math.sqrt(d[0]*d[0] + d[1]*d[1]);
+            if (prevDiagonal < curDiagonal || (prevDiagonal == curDiagonal && area < (d[0] * d[1]))) {
+                prevDiagonal = curDiagonal;
+                area = d[0] * d[1];
+            }
+        }
+        return area;
+    }
+}
