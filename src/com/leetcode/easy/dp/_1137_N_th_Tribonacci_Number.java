fc91ed5f5d3dc966fc664fc31e798e75cd2ba6df
alx <alx@alx.com>
2024-04-26 15:23:46 +0300

    https://leetcode.com/problems/n-th-tribonacci-number/

--- src/com/leetcode/easy/dp/_1137_N_th_Tribonacci_Number.java
+++ src/com/leetcode/easy/dp/_1137_N_th_Tribonacci_Number.java
@@ -1,6 +1,9 @@
 package com.leetcode.easy.dp;
 
 public class _1137_N_th_Tribonacci_Number {
+    //1137. N-th Tribonacci Number
+    //https://leetcode.com/problems/n-th-tribonacci-number/
+
     public int tribonacci(int n) {
         int n0 = 0;
         int n1 = 1;
@@ -19,5 +22,22 @@ public class _1137_N_th_Tribonacci_Number {
             n2 = nn;
         }
         return nn;
+    }
+
+    public int tribonacci1(int n) {
+        int t0 = 0, t1 = 1, t2 = 1;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+
+        for (int i = 3; i <= n; i++) {
+            int t = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = t;
+        }
+
+        return t2;
     }
 }
