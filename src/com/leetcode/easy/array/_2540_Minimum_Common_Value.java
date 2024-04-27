0bed4dbad0c3451815bf1808aae2845f4a45979f
alx <alx@alx.com>
2024-04-27 11:51:18 +0300

    https://leetcode.com/problems/minimum-common-value/

--- src/com/leetcode/easy/array/_2540_Minimum_Common_Value.java
+++ src/com/leetcode/easy/array/_2540_Minimum_Common_Value.java
@@ -2,7 +2,7 @@ package com.leetcode.easy.array;
 
 public class _2540_Minimum_Common_Value {
     //2540. Minimum Common Value
-    //https://leetcode.com/problems/minimum-common-value/
+    //https://leetcode.com/problems/minimum-common-value/description/
 
     public int getCommon(int[] nums1, int[] nums2) {
         int l1 = 0, l2 = 0;
@@ -10,6 +10,16 @@ public class _2540_Minimum_Common_Value {
             if (nums1[l1] == nums2[l2]) return nums1[l1];
             if (nums1[l1] < nums2[l2]) l1++;
             else if (nums1[l1] > nums2[l2]) l2++;
+        }
+        return -1;
+    }
+
+    public int getCommon1(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) return nums1[i];
+            if (nums1[i] > nums2[j]) j++;
+            else i++;
         }
         return -1;
     }
