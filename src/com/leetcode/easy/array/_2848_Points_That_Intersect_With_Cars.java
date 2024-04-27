f5d272b093eb6ad6bd21d6f99dcc6955e15a3eb4
alx <alx@alx.com>
2024-04-27 12:07:32 +0300

    https://leetcode.com/problems/points-that-intersect-with-cars/

--- src/com/leetcode/easy/array/_2848_Points_That_Intersect_With_Cars.java
+++ src/com/leetcode/easy/array/_2848_Points_That_Intersect_With_Cars.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class _2848_Points_That_Intersect_With_Cars {
     //2848. Points That Intersect With Cars
-    //https://leetcode.com/problems/points-that-intersect-with-cars/
+    //https://leetcode.com/problems/points-that-intersect-with-cars/description/
 
     public int numberOfPoints(List<List<Integer>> nums) {
         Set<Integer> set = new HashSet<>();
