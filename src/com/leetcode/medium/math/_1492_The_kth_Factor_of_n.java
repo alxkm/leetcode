dbd78251c304cd253c875adfea4542bd3ff623f1
alx <alx@alx.com>
2024-04-27 11:50:01 +0300

    https://leetcode.com/problems/the-kth-factor-of-n/

--- src/com/leetcode/medium/math/_1492_The_kth_Factor_of_n.java
+++ src/com/leetcode/medium/math/_1492_The_kth_Factor_of_n.java
@@ -2,7 +2,7 @@ package com.leetcode.medium.math;
 
 public class _1492_The_kth_Factor_of_n {
     //1492. The kth Factor of n
-    //https://leetcode.com/problems/the-kth-factor-of-n/
+    //https://leetcode.com/problems/the-kth-factor-of-n/description/
 
     public int kthFactor(int n, int k) {
         for (int i = 1, j = 0; i <= n; i++) {
