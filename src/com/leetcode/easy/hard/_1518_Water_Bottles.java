d9e132ce0380fd6e53801e36155586ae1a205ab4
Alex Klm <alexanderklmn@gmail.com>
2024-04-27 12:02:52 +0300

    https://leetcode.com/problems/water-bottles/

---
+++ src/com/leetcode/easy/hard/_1518_Water_Bottles.java
@@ -0,0 +1,23 @@
+package com.leetcode.easy.hard;
+
+public class _1518_Water_Bottles {
+    //https://leetcode.com/problems/water-bottles/
+    //1518. Water Bottles
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int cnt = 0;
+        while (numBottles > 0 && numBottles >= numExchange) {
+            int div = numBottles / numExchange;
+            cnt += div * numExchange;
+            numBottles = numBottles % numExchange;
+            numBottles += div;
+        }
+        return cnt + numBottles;
+    }
+
+    public static void main(String[] args) {
+        var sol = new _1518_Water_Bottles();
+        int numBottles = 9, numExchange = 3;
+        System.out.println(sol.numWaterBottles(numBottles, numExchange));
+    }
+}
