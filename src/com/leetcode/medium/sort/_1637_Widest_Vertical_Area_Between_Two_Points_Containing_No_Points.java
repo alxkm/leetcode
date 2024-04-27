a4a0b08eacc1e6c0a266c0fc2a848d135864601d
alx <alx@alx.com>
2024-04-27 11:53:18 +0300

    https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/

--- src/com/leetcode/medium/sort/_1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.java
+++ src/com/leetcode/medium/sort/_1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.java
@@ -3,7 +3,7 @@ package com.leetcode.medium.sort;
 import java.util.Arrays;
 
 public class _1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points {
-    //https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
+    //https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/description/
     //1637. Widest Vertical Area Between Two Points Containing No Points
 
     public int maxWidthOfVerticalArea(int[][] points) {
