a96017118ff639fe6c4d5f0fddf9641042ee8069
alx <alx@alx.com>
2024-04-27 11:49:03 +0300

    https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/

--- src/com/leetcode/easy/binarysearch/_1351_Count_Negative_Numbers_in_a_Sorted_Matrix.java
+++ src/com/leetcode/easy/binarysearch/_1351_Count_Negative_Numbers_in_a_Sorted_Matrix.java
@@ -2,7 +2,7 @@ package com.leetcode.easy.binarysearch;
 
 public class _1351_Count_Negative_Numbers_in_a_Sorted_Matrix {
     //1351. Count Negative Numbers in a Sorted Matrix
-    //https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+    //https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
     //binarySearch
 
     public int countNegatives(int[][] grid) {
