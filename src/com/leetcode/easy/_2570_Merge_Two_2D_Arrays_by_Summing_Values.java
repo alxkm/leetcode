815aa0216fbbd1eda5cfef124c8506cf7a14773f
Alex Klm <alexanderklmn@gmail.com>
2024-04-27 11:58:51 +0300

    https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/description/

---
+++ src/com/leetcode/easy/_2570_Merge_Two_2D_Arrays_by_Summing_Values.java
@@ -0,0 +1,42 @@
+package com.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _2570_Merge_Two_2D_Arrays_by_Summing_Values {
+    //2570. Merge Two 2D Arrays by Summing Values
+    //https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/description/
+
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        List<int[]> l = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i][0] == nums2[j][0]) {
+                l.add(new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
+            } else if (nums1[i][0] < nums2[j][0]) {
+                l.add(new int[]{nums1[i][0], nums1[i][1]});
+                i++;
+            } else {
+                l.add(new int[]{nums2[j][0], nums2[j][1]});
+                j++;
+            }
+        }
+
+        while (i < nums1.length) {
+            l.add(new int[]{nums1[i][0], nums1[i][1]});
+            i++;
+        }
+        while (j < nums2.length) {
+            l.add(new int[]{nums2[j][0], nums2[j][1]});
+            j++;
+        }
+
+        int[][] ar = new int[l.size()][2];
+        for (i = 0; i < l.size(); i++) {
+            ar[i] = l.get(i);
+        }
+        return ar;
+    }
+}
