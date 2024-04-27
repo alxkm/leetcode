93a973587a6c0f7c7d2bd87a5ce6225e1b91672c
alx <alx@alx.com>
2024-04-27 12:18:47 +0300

    https://leetcode.com/problems/find-if-path-exists-in-graph/

--- src/com/leetcode/easy/graph/_1971_Find_if_Path_Exists_in_Graph.java
+++ src/com/leetcode/easy/graph/_1971_Find_if_Path_Exists_in_Graph.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import java.util.Stack;
 
 public class _1971_Find_if_Path_Exists_in_Graph {
+    //1971. Find if Path Exists in Graph
+    //https://leetcode.com/problems/find-if-path-exists-in-graph/
+
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         if (source == destination) return true;
         Map<Integer, List<Integer>> map = new HashMap<>();
@@ -30,6 +33,30 @@ public class _1971_Find_if_Path_Exists_in_Graph {
                 if (i == destination) return true;
                 st.push(i);
             }
+        }
+        return false;
+    }
+
+    //new
+
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    public boolean validPath1(int n, int[][] edges, int source, int destination) {
+        if (source == destination) return true;
+        for (int[] v: edges) {
+            map.computeIfAbsent(v[0], s -> new ArrayList<>()).add(v[1]);
+            map.computeIfAbsent(v[1], s -> new ArrayList<>()).add(v[0]);
+        }
+
+        boolean[] visit = new boolean[n];
+        return dfs(source, visit, destination);
+    }
+
+    public boolean dfs(int a, boolean[] visit, int destination) {
+        if (visit[a]) return false;
+        visit[a] = true;
+        if (a == destination) return true;
+        for (int v: map.get(a)) {
+            if(dfs(v, visit, destination)) return true;
         }
         return false;
     }
