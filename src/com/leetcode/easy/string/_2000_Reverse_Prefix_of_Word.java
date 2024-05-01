aa030706a3ab8214019fbe5b01f1c977b29a68a9
alx <alx@alx.com>
2024-05-01 11:52:39 +0200

    https://leetcode.com/problems/reverse-prefix-of-word/

--- src/com/leetcode/easy/string/_2000_Reverse_Prefix_of_Word.java
+++ src/com/leetcode/easy/string/_2000_Reverse_Prefix_of_Word.java
@@ -1,6 +1,9 @@
 package com.leetcode.easy.string;
 
 public class _2000_Reverse_Prefix_of_Word {
+    //2000. Reverse Prefix of Word
+    //https://leetcode.com/problems/reverse-prefix-of-word/description/
+
     static public String reversePrefix(String word, char ch) {
         int index = 0;
         for (int i = 0; i < word.length(); i++) {
@@ -13,6 +16,15 @@ public class _2000_Reverse_Prefix_of_Word {
         StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
         sb.reverse();
         return sb + word.substring(index + 1);
+    }
+
+    public String reversePrefix1(String word, char ch) {
+        int index = word.indexOf(ch);
+        if (index == -1) return word;
+        StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
+        StringBuilder s1 = new StringBuilder(word.substring(index + 1, word.length()));
+        sb.reverse();
+        return sb.toString() + s1.toString();
     }
 
     public static void main(String[] args) {
