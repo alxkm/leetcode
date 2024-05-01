46a72ef96fd790f3c33c3f69971424dec72c4f10
alx <alx@alx.com>
2024-05-01 11:56:29 +0200

    https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/

---
+++ src/com/leetcode/medium/binary/_2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K.java
@@ -0,0 +1,20 @@
+package com.leetcode.medium.binary;
+
+public class _2997_Minimum_Number_of_Operations_to_Make_Array_XOR_Equal_to_K {
+    //2997. Minimum Number of Operations to Make Array XOR Equal to K
+    //https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/
+
+    public int minOperations(int[] nums, int k) {
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            n = n ^ nums[i];
+        }
+        int counter = 0;
+        while (k > 0 || n > 0) {
+            if ((k % 2) != (n % 2)) counter++;
+            k /= 2;
+            n /= 2;
+        }
+        return counter;
+    }
+}
