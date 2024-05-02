a9f5d1c77d42ae1acdaec3254d97aef7d52fd787
alx <alx@alx.com>
2024-05-02 11:55:14 +0300

    https://leetcode.com/problems/repeated-string-match/

---
+++ src/com/leetcode/medium/_686_Repeated_String_Match.java
@@ -0,0 +1,30 @@
+package com.leetcode.medium;
+
+public class _686_Repeated_String_Match {
+    //686. Repeated String Match
+    //https://leetcode.com/problems/repeated-string-match/description/
+
+    public int repeatedStringMatch(String a, String b) {
+        if (a.contains(b)) return 1;
+        int[] alp = new int[26];
+        int[] alp1 = new int[26];
+
+        for (char ch: a.toCharArray()) alp[ch - 'a']++;
+        for (char ch: b.toCharArray()) alp1[ch - 'a']++;
+
+        for (int i = 0; i < 26; i++) {
+            if (alp[i] == 0 && alp1[i] != 0) return -1;
+        }
+        StringBuilder sb = new StringBuilder(a);
+        sb.append(a);
+        int counter = 2;
+        while (true) {
+            if (sb.indexOf(b) != -1) return counter;
+            if (sb.length() > b.length() + a.length()) {
+                return -1;
+            }
+            sb.append(a);
+            counter++;
+        }
+    }
+}
