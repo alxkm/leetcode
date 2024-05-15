441aa59575712695087b089acd3c9e95ab849e4f
Alex Klimenko <alexanderklmn@gmail.com>
2024-05-15 10:38:09 +0300

    https://leetcode.com/problems/subarrays-with-k-different-integers/

---
+++ src/com/leetcode/hard/_992_Subarrays_with_K_Different_Integers.java
@@ -0,0 +1,27 @@
+package com.leetcode.hard;
+
+public class _992_Subarrays_with_K_Different_Integers {
+    //992. Subarrays with K Different Integers
+    //https://leetcode.com/problems/subarrays-with-k-different-integers/description/
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        int answer = 0, n = nums.length;
+        int[] cnt = new int[n + 1];
+        for (int i = 0, m = 0, j = 0; j < n; j++) {
+            cnt[nums[j]]++;
+            if (cnt[nums[j]] == 1) {
+                if (--k < 0) {
+                    cnt[nums[m++]] = 0;
+                    i = m;
+                }
+            }
+            if (k <= 0) {
+                while (cnt[nums[m]] > 1) {
+                    cnt[nums[m++]]--;
+                }
+                answer += m - i + 1;
+            }
+        }
+        return answer;
+    }
+}
