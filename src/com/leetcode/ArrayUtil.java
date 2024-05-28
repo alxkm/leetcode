d18a522a6dffed0fea15f50e864646390ff693c0
alx <alx@alx.com>
2024-05-28 19:56:30 +0300

    ArrayUtil

--- src/com/leetcode/ArrayUtil.java
+++ src/com/leetcode/ArrayUtil.java
@@ -1,10 +1,18 @@
 package com.leetcode;
 
 public class ArrayUtil {
-    public static int[] of(int ... a) {
+    public static int[] of(int... a) {
         return a;
     }
-    public static Integer[] ofInteger(Integer ... a) {
+    public static char[] of(char... a) {
+        return a;
+    }
+
+    public static Integer[] ofInteger(Integer... a) {
+        return a;
+    }
+
+    public static String[] of(String... a) {
         return a;
     }
 }
