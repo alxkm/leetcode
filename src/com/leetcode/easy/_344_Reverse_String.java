61a3f525f15b0cef28df774fd8da168fa2cf4175
alx <alx@alx.com>
2024-06-06 17:04:23 +0300

    https://leetcode.com/problems/reverse-string/

--- src/com/leetcode/easy/_344_Reverse_String.java
+++ src/com/leetcode/easy/_344_Reverse_String.java
@@ -1,6 +1,8 @@
 package com.leetcode.easy;
 
 public class _344_Reverse_String {
+    //344. Reverse String
+    //https://leetcode.com/problems/reverse-string/description/
 
     static public void reverseString(char[] s) {
         if (s.length == 1) return;
@@ -9,6 +11,14 @@ public class _344_Reverse_String {
             char ch = s[i];
             s[i] = s[s.length - i - 1];
             s[s.length - i - 1] = ch;
+        }
+    }
+
+    public void reverseString1(char[] s) {
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
         }
     }
 
