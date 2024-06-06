1739ce9add7590c1b4e3edb81a362eb0bc463d36
alx <alx@alx.com>
2024-06-06 16:57:07 +0300

    https://leetcode.com/problems/find-common-characters/

--- src/com/leetcode/easy/_1002_Find_Common_Characters.java
+++ src/com/leetcode/easy/_1002_Find_Common_Characters.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _1002_Find_Common_Characters {
-    static public List<String> commonChars(String[] words) {
+    //1002. Find Common Characters
+    //https://leetcode.com/problems/find-common-characters/description/
+
+    static public List<String> commonChars1(String[] words) {
         List<String> s = new ArrayList<>();
         String f = words[0];
 
@@ -27,8 +30,33 @@ public class _1002_Find_Common_Characters {
         return s;
     }
 
+    public static List<String> commonChars(String[] words) {
+        List<int[]> list = new ArrayList<>();
+
+        for (String s: words) {
+            char[] ch = s.toCharArray();
+            int[] ar = new int[26];
+            for (char c: ch) {
+                ar[c - 'a']++;
+            }
+            list.add(ar);
+        }
+        List<String> l = new ArrayList<>();
+        for (char ch: words[0].toCharArray()) {
+            int counter = 0;
+            for (int[] ar : list) {
+                if (ar[ch - 'a'] <= 0) break;
+                else ar[ch - 'a']--;
+                counter++;
+            }
+            if (counter == list.size()) l.add(String.valueOf(ch));
+        }
+        return l;
+    }
+
     public static void main(String[] args) {
-        String[] s = {"cool","lock","cook"};
+        //String[] s = {"cool","lock","cook"};
+        String[] s = {"bella","label","roller"};
 
         System.out.println(commonChars(s));
     }
