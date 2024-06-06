30a8d55f8f4cd56c882004792881e6f64808a6f0
alx <alx@alx.com>
2024-06-06 16:53:31 +0300

    https://leetcode.com/problems/special-array-i/

---
+++ src/com/leetcode/easy/array/_3151_Special_Array_I.java
@@ -0,0 +1,22 @@
+package com.leetcode.easy.array;
+
+import com.leetcode.ArrayUtil;
+
+public class _3151_Special_Array_I {
+    //3151. Special Array I
+    //https://leetcode.com/problems/special-array-i/description/
+
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] % 2 == 0 && nums[i] % 2 != 0) continue;
+            else if (nums[i - 1] % 2 != 0 && nums[i] % 2 == 0) continue;
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        var sol = new _3151_Special_Array_I();
+        System.out.println(sol.isArraySpecial(ArrayUtil.of(2, 1, 4)));
+    }
+}
