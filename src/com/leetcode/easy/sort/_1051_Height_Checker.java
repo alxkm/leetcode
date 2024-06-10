2d623500f6657c6fb5764531e2475546742ce88b
alx <alx@alx.com>
2024-06-10 21:27:50 +0200

    https://leetcode.com/problems/height-checker/

--- src/com/leetcode/easy/sort/_1051_Height_Checker.java
+++ src/com/leetcode/easy/sort/_1051_Height_Checker.java
@@ -3,6 +3,9 @@ package com.leetcode.easy.sort;
 import java.util.Arrays;
 
 public class _1051_Height_Checker {
+    //1051. Height Checker
+    //https://leetcode.com/problems/height-checker/description/
+
     public int heightChecker(int[] heights) {
         int[] expected = Arrays.copyOf(heights, heights.length);
         Arrays.sort(expected);
@@ -11,5 +14,15 @@ public class _1051_Height_Checker {
             if (heights[i] != expected[i]) cnt++;
         }
         return cnt;
+    }
+
+    public int heightChecker1(int[] heights) {
+        int[] copy = heights.clone();
+        Arrays.sort(copy);
+        int counter = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != copy[i]) counter++;
+        }
+        return counter;
     }
 }
