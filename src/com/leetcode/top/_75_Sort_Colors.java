860d6a6561351c33d6e6a01963b939ced953aec3
alx <alx@alx.com>
2024-06-13 17:40:15 +0300

    https://leetcode.com/problems/sort-colors/

--- src/com/leetcode/top/_75_Sort_Colors.java
+++ src/com/leetcode/top/_75_Sort_Colors.java
@@ -5,6 +5,14 @@ public class _75_Sort_Colors {
     //https://leetcode.com/problems/sort-colors/
 
     public void sortColors(int[] nums) {
+        int[] cl = new int[3];
+        for (int n: nums) cl[n]++;
+        for (int i = 0; i < cl[0]; i++) nums[i] = 0;
+        for (int i = cl[0]; i < cl[0] + cl[1]; i++) nums[i] = 1;
+        for (int i = cl[0] + cl[1]; i < nums.length; i++) nums[i] = 2;
+    }
+
+    public void sortColors0(int[] nums) {
         int[] pos = new int[]{0, 0, nums.length - 1};
 
         for (int i = 0; i < nums.length; i++) {
