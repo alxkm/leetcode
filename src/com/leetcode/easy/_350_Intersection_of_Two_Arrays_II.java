74523b4238d7e2b272ca7e248cc05bf1fb408777
Alex Klymenko <alx@alx.com>
2024-07-03 00:04:47 +0200

    https://leetcode.com/problems/intersection-of-two-arrays-ii

--- src/com/leetcode/easy/_350_Intersection_of_Two_Arrays_II.java
+++ src/com/leetcode/easy/_350_Intersection_of_Two_Arrays_II.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class _350_Intersection_of_Two_Arrays_II {
+    //350. Intersection of Two Arrays II
+    //https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+
     static public int[] intersect(int[] nums1, int[] nums2) {
         var l1 = new ArrayList<Integer>(nums1.length);
         var l2 = new ArrayList<Integer>(nums2.length);
@@ -29,6 +32,30 @@ public class _350_Intersection_of_Two_Arrays_II {
             l4[i] = l3.get(i);
         }
         return l4;
+    }
+
+    public int[] intersect1(int[] nums1, int[] nums2) {
+        int[] in1 = new int[1001];
+        int[] in2 = new int[1001];
+
+        for (int n: nums1) in1[n]++;
+        for (int n: nums2) {
+            if (in1[n] > 0) {
+                in2[n]++;
+                in1[n]--;
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < in2.length; i++) {
+            for (int j = 0; j < in2[i]; j++) {
+                list.add(i);
+            }
+        }
+        int[] ar = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ar[i] = list.get(i);
+        }
+        return ar;
     }
 
     public static void main(String[] args) {
