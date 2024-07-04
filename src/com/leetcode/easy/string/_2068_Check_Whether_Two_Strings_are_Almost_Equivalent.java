f2adf3eec87a2f5da3c5b3229b7bdea4a1fb4e1e
alx <alx@alx.com>
2024-07-04 16:26:40 +0300

    https://leetcode.com/problems/check-whether-two-strings-are-almost-equivalent

--- src/com/leetcode/easy/string/_2068_Check_Whether_Two_Strings_are_Almost_Equivalent.java
+++ src/com/leetcode/easy/string/_2068_Check_Whether_Two_Strings_are_Almost_Equivalent.java
@@ -1,33 +1,28 @@
 package com.leetcode.easy.string;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class _2068_Check_Whether_Two_Strings_are_Almost_Equivalent {
-    static     public boolean checkAlmostEquivalent(String word1, String word2) {
-        Set<Character> s = new HashSet<>();
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i < word1.length(); i++) {
-            map.put(word1.charAt(i), map.getOrDefault(word1.charAt(i), 0) + 1);
-            s.add(word1.charAt(i));
+    //https://leetcode.com/problems/check-whether-two-strings-are-almost-equivalent/description/
+    //2068. Check Whether Two Strings are Almost Equivalent
+
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int[] w1 = new int[26];
+        int[] w2 = new int[26];
+
+        for (char ch : word1.toCharArray()) {
+            w1[ch - 'a']++;
         }
-        Map<Character, Integer> map1 = new HashMap<>();
-        for(int i = 0; i < word1.length(); i++) {
-            map.put(word1.charAt(i), map.getOrDefault(word1.charAt(i), 0) + 1);
-            s.add(word2.charAt(i));
+        for (char ch : word2.toCharArray()) {
+            w2[ch - 'a']++;
         }
-        for (Character c : s) {
-            if (Math.abs(map.getOrDefault(c, 0) - map1.getOrDefault(c, 0)) > 3) {
-                return false;
-            }
+
+        for (int i = 0; i < w1.length; i++) {
+            if (Math.abs(w1[i] - w2[i]) >= 4) return false;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(checkAlmostEquivalent("abcdeef",
-                                                 "abaaacc"));
+        var sol = new _2068_Check_Whether_Two_Strings_are_Almost_Equivalent();
+        System.out.println(sol.checkAlmostEquivalent("abcdeef", "abaaacc"));
     }
 }
