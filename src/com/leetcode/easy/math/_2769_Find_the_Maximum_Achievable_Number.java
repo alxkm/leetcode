b866999d0aec453874f5bdcb1195f06e9c21fba7
Alex Klymenko <alx@alx.com>
2024-07-07 14:02:24 +0200

    https://leetcode.com/problems/find-the-maximum-achievable-number

---
+++ src/com/leetcode/easy/math/_2769_Find_the_Maximum_Achievable_Number.java
@@ -0,0 +1,10 @@
+package com.leetcode.easy.math;
+
+public class _2769_Find_the_Maximum_Achievable_Number {
+    //2769. Find the Maximum Achievable Number
+    //https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+    public int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
+    }
+}
