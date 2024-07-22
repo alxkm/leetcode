097ddaa5bfaa3974dea3e179f92334b6a18b234b
alx <alx@alx.com>
2024-07-22 17:46:20 +0300

    https://leetcode.com/problems/sort-the-people

--- src/com/leetcode/easy/array/_2418_Sort_the_People.java
+++ src/com/leetcode/easy/array/_2418_Sort_the_People.java
@@ -1,9 +1,13 @@
 package com.leetcode.easy.array;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class _2418_Sort_the_People {
+    //2418. Sort the People
+    //https://leetcode.com/problems/sort-the-people/description/
+
     public String[] sortPeople(String[] names, int[] heights) {
         Map<Integer, String> map = new HashMap<>();
         for (int i = 0; i < names.length; i++) {
@@ -25,5 +29,21 @@ public class _2418_Sort_the_People {
             map.remove(key);
         }
         return names;
+    }
+
+    public String[] sortPeople1(String[] names, int[] heights) {
+        Map<Integer, String> map = new HashMap<>();
+        int[][] array = new int[names.length][2];
+        for (int i = 0; i < names.length; i++) {
+            map.put(i, names[i]);
+            array[i][0] = i;
+            array[i][1] = heights[i];
+        }
+        Arrays.sort(array, (x, y) -> y[1] - x[1]);
+        String[] ar = new String[names.length];
+        for (int i = 0; i < names.length; i++) {
+            ar[i] = map.get(array[i][0]);
+        }
+        return ar;
     }
 }
