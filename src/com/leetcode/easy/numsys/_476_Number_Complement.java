2a6fb9db06984276f8c4a23c5bc1c24693baba26
Alex Klymenko <alexanderklmn@gmail.com>
2024-09-02 15:49:55 +0200

    https://leetcode.com/problems/number-complement TOP easy

--- src/com/leetcode/easy/numsys/_476_Number_Complement.java
+++ src/com/leetcode/easy/numsys/_476_Number_Complement.java
@@ -1,9 +1,12 @@
-package com.leetcode.easy.top.array;
+package com.leetcode.easy.numsys;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class _476_Number_Complement {
+    //476. Number Complement
+    //https://leetcode.com/problems/number-complement/description/
+
     static public int findComplement(int num) {
         int a = 0;
 
@@ -18,6 +21,20 @@ public class _476_Number_Complement {
         }
 
         return a;
+    }
+
+    public int findComplement1(int num) {
+        int s = 0;
+        int two = 1;
+
+        while (num > 0) {
+            int r = (num % 2 == 1) ? 0 : 1;
+            num /= 2;
+            s += r * two;
+            two *= 2;
+        }
+
+        return s;
     }
 
     public static void main(String[] args) {
