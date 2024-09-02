0895d49244cb7bb9a9bc7245d9f04ccb94ab23b9
Alex Klymenko <alexanderklmn@gmail.com>
2024-09-02 15:29:01 +0200

    https://leetcode.com/problems/fraction-addition-and-subtraction TODO

---
+++ src/com/leetcode/medium/implementation/_592_Fraction_Addition_and_Subtraction.java
@@ -0,0 +1,68 @@
+package com.leetcode.medium.implementation;
+
+public class _592_Fraction_Addition_and_Subtraction {
+    //592. Fraction Addition and Subtraction
+    //https://leetcode.com/problems/fraction-addition-and-subtraction/
+    //FOREIGN_SOLUTION
+
+    public String fractionAddition(String expression) {
+        int num = 0;
+        int denom = 1;
+
+        int i = 0;
+        while (i < expression.length()) {
+            int currNum = 0;
+            int currDenom = 0;
+
+            boolean isNegative = false;
+
+            // check for sign
+            if (expression.charAt(i) == '-' || expression.charAt(i) == '+') {
+                if (expression.charAt(i) == '-') {
+                    isNegative = true;
+                }
+                // move to next character
+                i++;
+            }
+
+            // build numerator
+            while (Character.isDigit(expression.charAt(i))) {
+                int val = expression.charAt(i) - '0';
+                currNum = currNum * 10 + val;
+                i++;
+            }
+
+            if (isNegative) currNum *= -1;
+
+            //skip divisor
+            i++;
+
+            // build denominator
+            while (
+                    i < expression.length() &&
+                            Character.isDigit(expression.charAt(i))
+            ) {
+                int val = expression.charAt(i) - '0';
+                currDenom = currDenom * 10 + val;
+                i++;
+            }
+
+            // add fractions together using common denominator
+            num = num * currDenom + currNum * denom;
+            denom = denom * currDenom;
+        }
+
+        int gcd = Math.abs(FindGCD(num, denom));
+
+        // reduce fractions
+        num /= gcd;
+        denom /= gcd;
+
+        return num + "/" + denom;
+    }
+
+    private int FindGCD(int a, int b) {
+        if (a == 0) return b;
+        return FindGCD(b % a, a);
+    }
+}
