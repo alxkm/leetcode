98892bcaa32e3183a294c6bc07f5e73946863a2c
Alex Klymenko <alexanderklmn@gmail.com>
2024-09-02 16:53:20 +0200

    https://leetcode.com/problems/spiral-matrix-iii todo almost (#725)

---
+++ src/com/leetcode/medium/matrix/_885_Spiral_Matrix_III.java
@@ -0,0 +1,41 @@
+package com.leetcode.medium.matrix;
+
+public class _885_Spiral_Matrix_III {
+    //885. Spiral Matrix III
+    //https://leetcode.com/problems/spiral-matrix-iii/description/
+
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        // Store all possible directions in an array.
+        int[][] dir = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        int[][] traversed = new int[rows * cols][2];
+        int idx = 0;
+
+        // Initial step size is 1, value of d represents the current direction.
+        for (int step = 1, direction = 0; idx < rows * cols;) {
+            // direction = 0 -> East, direction = 1 -> South
+            // direction = 2 -> West, direction = 3 -> North
+            for (int i = 0; i < 2; ++i) {
+                for (int j = 0; j < step; ++j) {
+                    // Validate the current position
+                    if (
+                            rStart >= 0 &&
+                                    rStart < rows &&
+                                    cStart >= 0 &&
+                                    cStart < cols
+                    ) {
+                        traversed[idx][0] = rStart;
+                        traversed[idx][1] = cStart;
+                        ++idx;
+                    }
+                    // Make changes to the current position.
+                    rStart += dir[direction][0];
+                    cStart += dir[direction][1];
+                }
+
+                direction = (direction + 1) % 4;
+            }
+            ++step;
+        }
+        return traversed;
+    }
+}
