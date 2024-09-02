43aaa9c9ec9292e25bbd31ead919f45159c2958b
Alex Klymenko <alexanderklmn@gmail.com>
2024-09-02 15:22:15 +0200

    https://leetcode.com/problems/binary-tree-postorder-traversal

--- src/com/leetcode/easy/tree/_145_Binary_Tree_Postorder_Traversal.java
+++ src/com/leetcode/easy/tree/_145_Binary_Tree_Postorder_Traversal.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _145_Binary_Tree_Postorder_Traversal {
+    //https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+    //145. Binary Tree Postorder Traversal
+
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         postorderTraversal(root, list);
@@ -17,6 +20,21 @@ public class _145_Binary_Tree_Postorder_Traversal {
             postorderTraversal(root.left, list);
             postorderTraversal(root.right, list);
             list.add(root.val);
+        }
+    }
+
+    class Solution {
+        public List<Integer> postorderTraversal(TreeNode root) {
+            List<Integer> l = new ArrayList<>();
+            dfs(root, l);
+            return l;
+        }
+
+        private void dfs(TreeNode t, List<Integer> l) {
+            if (t == null) return;
+            dfs(t.left, l);
+            dfs(t.right, l);
+            l.add(t.val);
         }
     }
 }
