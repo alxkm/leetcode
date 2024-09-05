cc343774f1d9d4215e7cd7800deb32a14e7e1543
alx <alx@alx.com>
2024-09-05 22:15:40 +0300

    https://leetcode.com/problems/find-k-th-smallest-pair-distance TODO hard binsearch

--- src/com/leetcode/hard/_719_Find_K_th_Smallest_Pair_Distance.java
+++ src/com/leetcode/hard/_719_Find_K_th_Smallest_Pair_Distance.java
@@ -6,7 +6,8 @@ import java.util.TreeMap;
 
 public class _719_Find_K_th_Smallest_Pair_Distance {
     //719. Find K-th Smallest Pair Distance
-    //https://leetcode.com/problems/find-k-th-smallest-pair-distance/description/
+    //https://leetcode.com/problems/find-k-th-smallest-pair-distance/editorial/?envType=daily-question&envId=2024-08-14
+    //foreign_solution
 
     public int smallestDistancePair(int[] nums, int k) {
         Arrays.sort(nums);
@@ -47,22 +48,5 @@ public class _719_Find_K_th_Smallest_Pair_Distance {
             count += right - left;
         }
         return count;
-    }
-
-    class Solution {
-        public int smallestDistancePair(int[] nums, int k) {
-            Arrays.sort(nums);
-            Map<Integer, Integer> map = new TreeMap<>();
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    map.put(Math.abs(nums[i] - nums[j]), map.getOrDefault(Math.abs(nums[i] - nums[j]), 0) + 1);
-                }
-            }
-            for (var entry: map.entrySet()) {
-                if (entry.getValue() >= k) return entry.getKey();
-                k -= entry.getValue();
-            }
-            return 1;
-        }
     }
 }
