594af703cd9c46799e2f621f4ee84b03a98da481
alx <alx@alx.com>
2024-09-12 15:45:09 +0300

    https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list

--- src/com/leetcode/medium/list/_2807_Insert_Greatest_Common_Divisors_in_Linked_List.java
+++ src/com/leetcode/medium/list/_2807_Insert_Greatest_Common_Divisors_in_Linked_List.java
@@ -3,7 +3,7 @@ package com.leetcode.medium.list;
 import com.leetcode.ListNode;
 
 public class _2807_Insert_Greatest_Common_Divisors_in_Linked_List {
-    //https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/
+    //https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/description/
     //2807. Insert Greatest Common Divisors in Linked List
 
     public ListNode insertGreatestCommonDivisors(ListNode head) {
@@ -25,5 +25,26 @@ public class _2807_Insert_Greatest_Common_Divisors_in_Linked_List {
         while (b != 0)
             b = a % (a = b);
         return a;
+    }
+}
+
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode h = head;
+        ListNode prev = head;
+        h = h.next;
+        while (h != null) {
+            int d = gcd(h.val, prev.val);
+            prev.next = new ListNode(d, h);
+            prev = h;
+            h = h.next;
+        }
+
+        return head;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 }
