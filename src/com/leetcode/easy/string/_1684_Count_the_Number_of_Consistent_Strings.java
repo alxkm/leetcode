a6d2932cdbded1b6ce92c75dac45804a4bbde282
alx <alx@alx.com>
2024-09-17 18:12:32 +0300

    https://leetcode.com/problems/count-the-number-of-consistent-strings

--- src/com/leetcode/easy/string/_1684_Count_the_Number_of_Consistent_Strings.java
+++ src/com/leetcode/easy/string/_1684_Count_the_Number_of_Consistent_Strings.java
@@ -1,8 +1,12 @@
 package com.leetcode.easy.string;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class _1684_Count_the_Number_of_Consistent_Strings {
+    //1684. Count the Number of Consistent Strings
+    //https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
+
     static public int countConsistentStrings(String allowed, String[] words) {
         int counter = 0;
 
@@ -21,6 +25,23 @@ public class _1684_Count_the_Number_of_Consistent_Strings {
                 }
             }
             if (consist) counter++;
+        }
+        return counter;
+    }
+
+    public int countConsistentStrings1(String allowed, String[] words) {
+        Set<Character> set = new HashSet<>();
+        for (char ch: allowed.toCharArray()) set.add(ch);
+        int counter = 0;
+        for (String s: words) {
+            boolean isConsistent = true;
+            for (char ch: s.toCharArray()) {
+                if (!set.contains(ch)) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+            if (isConsistent) counter++;
         }
         return counter;
     }
