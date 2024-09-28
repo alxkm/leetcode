64c4eba8ba82c231c2b5ee25184b26049c1674e9
Alex Klymenko <alexanderklmn@gmail.com>
2024-09-28 12:51:57 +0200

    https://leetcode.com/problems/find-missing-observations

--- src/com/leetcode/easy/string/additional/_1945_Sum_of_Digits_of_String_After_Convert.java
+++ src/com/leetcode/easy/string/additional/_1945_Sum_of_Digits_of_String_After_Convert.java
@@ -1,6 +1,9 @@
 package com.leetcode.easy.string.additional;
 
 public class _1945_Sum_of_Digits_of_String_After_Convert {
+    //1945. Sum of Digits of String After Convert
+    //https://leetcode.com/problems/sum-of-digits-of-string-after-convert/description/?envType=daily-question&envId=2024-09-03
+
     static public int getLucky(String s, int k) {
         StringBuilder sb = new StringBuilder();
 
@@ -15,6 +18,26 @@ public class _1945_Sum_of_Digits_of_String_After_Convert {
             sb = new StringBuilder(Integer.valueOf(sum).toString());
         }
         return sum;
+    }
+
+    class Solution {
+        public int getLucky(String s, int k) {
+            StringBuilder sb = new StringBuilder();
+            for (char ch: s.toCharArray()) {
+                sb.append(ch - 'a' + 1);
+            }
+            int n = 0;
+            while (k > 0) {
+                int t = 0;
+                for (char ch: sb.toString().toCharArray()) {
+                    t += ch - '0';
+                }
+                sb = new StringBuilder(Integer.valueOf(t).toString());
+                n = t;
+                k--;
+            }
+            return n;
+        }
     }
 
     public static void main(String[] args) {
