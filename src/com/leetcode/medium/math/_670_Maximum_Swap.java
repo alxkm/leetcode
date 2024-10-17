1195512736a81bb364a1b2316756cb4b0f885e01
alx <alx@alx.com>
2024-10-17 16:30:54 +0300

    https://leetcode.com/problems/maximum-swap

--- src/com/leetcode/medium/math/_670_Maximum_Swap.java
+++ src/com/leetcode/medium/math/_670_Maximum_Swap.java
@@ -1,12 +1,36 @@
 package com.leetcode.medium.math;
 
+import java.util.Arrays;
+
 public class _670_Maximum_Swap {
     //https://leetcode.com/problems/maximum-swap/
+    //https://leetcode.com/problems/maximum-swap/?envType=daily-question&envId=2024-10-17
     //670. Maximum Swap
     //TOP
     //FOREIGN_SOLUTION
 
     public int maximumSwap(int num) {
+        char[] ch = Integer.valueOf(num).toString().toCharArray();
+        int[][] ar = new int[ch.length][2];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = new int[] {ch[i], i};
+        }
+        Arrays.sort(ar, (x, y) -> y[0] - x[0]);
+        System.out.println(Arrays.deepToString(ar));
+        for (int i = 0; i < ar.length; i++) {
+            if (ch[i] != ar[i][0]) {
+                int j = i;
+                while (j + 1 < ar.length && ar[j][0] == ar[j + 1][0]) j++;
+                char c = ch[i];
+                ch[i] = (char)ar[j][0];
+                ch[ar[j][1]] = c;
+                break;
+            }
+        }
+        return Integer.parseInt(new String(ch));
+    }
+
+    public int maximumSwap1(int num) {
         if (num <= 11) return num;
         String s = num + "";
         boolean gt = false;
