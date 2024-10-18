4383b9158ef90e482a94a0f8d8bcc28ac1bd7b33
Alex Klymenko <alexanderklmn@gmail.com>
2024-10-18 14:24:22 +0200

    https://leetcode.com/problems/minimum-string-length-after-removing-substrings

---
+++ src/com/leetcode/easy/stackqueue/_2696_Minimum_String_Length_After_Removing_Substrings.java
@@ -0,0 +1,22 @@
+package com.leetcode.easy.stackqueue;
+
+public class _2696_Minimum_String_Length_After_Removing_Substrings {
+    //2696. Minimum String Length After Removing Substrings
+    //https://leetcode.com/problems/minimum-string-length-after-removing-substrings/description/
+    //not optimal
+
+    public int minLength(String s) {
+        for (int i = 0; i < 30; i++) {
+            while (s.indexOf("AB") != -1) {
+                int index = s.indexOf("AB");
+                s = s.substring(0, index) + s.substring(index + 2);
+            }
+
+            while (s.indexOf("CD") != -1) {
+                int index = s.indexOf("CD");
+                s = s.substring(0, index) + s.substring(index + 2);
+            }
+        }
+        return s.length();
+    }
+}
