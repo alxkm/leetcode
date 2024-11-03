b68e3a719a419bd886ef0969c0c6b9adde16777c
Alex Klymenko <alexanderklmn@gmail.com>
2024-11-03 17:24:30 +0100

    https://leetcode.com/problems/rotate-string

--- src/com/leetcode/easy/string/_796_Rotate_String.java
+++ src/com/leetcode/easy/string/_796_Rotate_String.java
@@ -1,9 +1,17 @@
 package com.leetcode.easy.string;
 
 public class _796_Rotate_String {
+    //796. Rotate String
+    //https://leetcode.com/problems/rotate-string
+
     public boolean rotateString(String s, String goal) {
         if (s.length() != goal.length()) return false;
         s = s + s;
         return s.contains(goal);
+    }
+
+    public boolean rotateString1(String s, String goal) {
+        String str = s + s;
+        return s.length() <= goal.length() && str.contains(goal);
     }
 }
