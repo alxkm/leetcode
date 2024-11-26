98a7da587ee81a86c7ff10c5ae6cc6de22b07fd6
alx <alx@alx.com>
2024-11-26 17:14:04 +0200

    https://leetcode.com/problems/circular-sentence

--- src/com/leetcode/easy/string/_2490_Circular_Sentence.java
+++ src/com/leetcode/easy/string/_2490_Circular_Sentence.java
@@ -1,6 +1,7 @@
 package com.leetcode.easy.string;
 
 public class _2490_Circular_Sentence {
+    //2490. Circular Sentence
     //https://leetcode.com/problems/circular-sentence/
 
     public boolean isCircularSentence(String sentence) {
@@ -13,5 +14,16 @@ public class _2490_Circular_Sentence {
         }
 
         return a[j].charAt(a[j].length() - 1) == a[0].charAt(0);
+    }
+
+    public boolean isCircularSentence1(String sentence) {
+        if (sentence.charAt(0) != sentence.charAt(sentence.length() - 1)) return false;
+        String[] words = sentence.split(" ");
+        char prev = words[0].charAt(words[0].length() - 1);
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].charAt(0) != prev) return false;
+            prev = words[i].charAt(words[i].length() - 1);
+        }
+        return true;
     }
 }
