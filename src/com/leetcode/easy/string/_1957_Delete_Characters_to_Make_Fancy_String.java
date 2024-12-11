913d130f0701305e67d7a745772b9cf91499eb7e
Alex Klymenko <alexanderklmn@gmail.com>
2024-12-11 17:16:29 +0100

    https://leetcode.com/problems/delete-characters-to-make-fancy-string

--- src/com/leetcode/easy/string/_1957_Delete_Characters_to_Make_Fancy_String.java
+++ src/com/leetcode/easy/string/_1957_Delete_Characters_to_Make_Fancy_String.java
@@ -2,7 +2,7 @@ package com.leetcode.easy.string;
 
 public class _1957_Delete_Characters_to_Make_Fancy_String {
     //1957. Delete Characters to Make Fancy String
-    //https://leetcode.com/problems/delete-characters-to-make-fancy-string/
+    //https://leetcode.com/problems/delete-characters-to-make-fancy-string/description/
 
     public String makeFancyString(String s) {
         StringBuilder sb = new StringBuilder();
@@ -23,6 +23,20 @@ public class _1957_Delete_Characters_to_Make_Fancy_String {
                 prev = ch[i];
                 sb.append(ch[i]);
             }
+        }
+        return sb.toString();
+    }
+
+    public String makeFancyString1(String s) {
+        if (s.length() < 3) return s;
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+        sb.append(s.charAt(1));
+        for (int i = 2; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1) && s.charAt(i) == s.charAt(i - 2)) {
+                continue;
+            }
+            sb.append(s.charAt(i));
         }
         return sb.toString();
     }
