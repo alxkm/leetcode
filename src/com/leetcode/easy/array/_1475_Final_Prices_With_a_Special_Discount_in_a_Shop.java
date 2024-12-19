1e7636914da613b926c0bd9fe82e9519467075ad
alx <alx@alx.com>
2024-12-19 11:22:37 +0200

    https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop

--- src/com/leetcode/easy/array/_1475_Final_Prices_With_a_Special_Discount_in_a_Shop.java
+++ src/com/leetcode/easy/array/_1475_Final_Prices_With_a_Special_Discount_in_a_Shop.java
@@ -3,6 +3,9 @@ package com.leetcode.easy.array;
 import java.util.Arrays;
 
 public class _1475_Final_Prices_With_a_Special_Discount_in_a_Shop {
+    //1475. Final Prices With a Special Discount in a Shop
+    //https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/description/?envType=daily-question&envId=2024-12-18
+
     static public int[] finalPrices(int[] prices) {
         for (int i = 0; i < prices.length - 1; i++) {
             int discount = -1;
@@ -16,6 +19,18 @@ public class _1475_Final_Prices_With_a_Special_Discount_in_a_Shop {
             }
             if (discount != -1) {
                 prices[i] -= discount;
+            }
+        }
+        return prices;
+    }
+
+    public int[] finalPrices1(int[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] <= prices[i]) {
+                    prices[i] = prices[i] - prices[j];
+                    break;
+                }
             }
         }
         return prices;
