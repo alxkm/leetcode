593d441a621cce51867075ce033fa6851e3a80c8
alx <alx@alx.com>
2025-01-14 16:39:21 +0200

    https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays

--- src/com/leetcode/easy/array/_2657_Find_the_Prefix_Common_Array_of_Two_Arrays.java
+++ src/com/leetcode/easy/array/_2657_Find_the_Prefix_Common_Array_of_Two_Arrays.java
@@ -4,8 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class _2657_Find_the_Prefix_Common_Array_of_Two_Arrays {
-
     //https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/
+    //https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/?envType=daily-question&envId=2025-01-14
     //2657. Find the Prefix Common Array of Two Arrays
 
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
@@ -30,4 +30,21 @@ public class _2657_Find_the_Prefix_Common_Array_of_Two_Arrays {
         return dp;
     }
 
+    public int[] findThePrefixCommonArray1(int[] A, int[] B) {
+        boolean[] set1 = new boolean[51];
+        boolean[] set2 = new boolean[51];
+        int[] ans = new int[A.length];
+        for (int i = 0; i < A.length && i < B.length; i++) {
+            set1[A[i]] = true;
+            set2[B[i]] = true;
+
+            int counter = 0;
+            for (int j = 0; j < 51; j++) {
+                if (set1[j] && set1[j] == set2[j]) counter++;
+            }
+
+            ans[i] = counter;
+        }
+        return ans;
+    }
 }
