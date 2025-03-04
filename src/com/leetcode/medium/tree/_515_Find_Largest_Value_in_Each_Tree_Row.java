79f883968cdde4030e43daea486181359ad672f6
alx <alx@alx.com>
2025-03-04 18:00:16 +0200

    https://leetcode.com/problems/find-largest-value-in-each-tree-row

--- src/com/leetcode/medium/tree/_515_Find_Largest_Value_in_Each_Tree_Row.java
+++ src/com/leetcode/medium/tree/_515_Find_Largest_Value_in_Each_Tree_Row.java
@@ -30,4 +30,67 @@ public class _515_Find_Largest_Value_in_Each_Tree_Row {
         }
         return l;
     }
+
+    public List<Integer> largestValues1(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<Integer> l = new ArrayList<>();
+        if (root == null) return l;
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int e = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode t = q.poll();
+                e = Math.max(e, t.val);
+                if (t.left != null) q.offer(t.left);
+                if (t.right != null) q.offer(t.right);
+            }
+            l.add(e);
+        }
+        return l;
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public List<Integer> largestValues(TreeNode root) {
+            if (root == null) {
+                return new ArrayList<>();
+            }
+
+            List<Integer> res = new ArrayList<>();
+            findLargestValues(root, 0, res);
+            return res;
+        }
+
+        private void findLargestValues(TreeNode root, int level, List<Integer> largestValues) {
+            if (root == null) {
+                return;
+            }
+
+            if (level > largestValues.size() - 1) {
+                largestValues.add(root.val);
+            } else {
+                int currMax = largestValues.get(level);
+                int newMax = Math.max(currMax, root.val);
+                largestValues.set(level, newMax);
+            }
+
+            findLargestValues(root.left, level + 1, largestValues);
+            findLargestValues(root.right, level + 1, largestValues);
+        }
+    }
 }
