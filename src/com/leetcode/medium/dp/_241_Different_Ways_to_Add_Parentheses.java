956f0d3cd470d181dccd47f07e67c804275aa5ff
Alex Klymenko <alexanderklmn@gmail.com>
2025-03-11 22:38:31 +0100

    https://leetcode.com/problems/different-ways-to-add-parentheses

--- src/com/leetcode/medium/dp/_241_Different_Ways_to_Add_Parentheses.java
+++ src/com/leetcode/medium/dp/_241_Different_Ways_to_Add_Parentheses.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class _241_Different_Ways_to_Add_Parentheses {
     //241. Different Ways to Add Parentheses
@@ -53,5 +54,35 @@ public class _241_Different_Ways_to_Add_Parentheses {
 
         dp.put(s, list);
         return list;
+    }
+
+    public static int[] smallestRange(List<List<Integer>> nums) {
+        Map<Integer, List<Integer>> map = new TreeMap<>();
+        int k = nums.size();
+        for (var l: nums) {
+            for (int n: l) {
+                map.computeIfAbsent(n, key -> new ArrayList<>()).add(n);
+            }
+        }
+        List<Integer> allk = new ArrayList<>();
+
+        for (var key: map.keySet()) {
+            if (map.get(key).size() == k) allk.add(key);
+        }
+
+        int[] min = new int[2];
+        int distance = Integer.MAX_VALUE;
+        for (int i = 0; i < allk.size() - 1; i++) {
+            if (distance > allk.get(i) - allk.get(i + 1)) {
+                min[0] = allk.get(i);
+                min[1] = allk.get(i + 1);
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        smallestRange(List.of(List.of(4,10,15,24,26), List.of(0,9,12,20), List.of(5,18,22,30)));
     }
 }
