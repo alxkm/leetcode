4b531be196dc9d6086a6e1fb295f1e1fd1517d98
alx <alx@alx.com>
2025-03-17 13:31:48 +0200

    https://leetcode.com/problems/maximum-ascending-subarray-sum

--- src/com/leetcode/easy/array/_1800_Maximum_Ascending_Subarray_Sum.java
+++ src/com/leetcode/easy/array/_1800_Maximum_Ascending_Subarray_Sum.java
@@ -1,6 +1,9 @@
 package com.leetcode.easy.array;
 
 public class _1800_Maximum_Ascending_Subarray_Sum {
+    //1800. Maximum Ascending Subarray Sum
+    //https://leetcode.com/problems/maximum-ascending-subarray-sum/description/
+
     public int maxAscendingSum(int[] nums) {
         int a = nums[0];
         int max = nums[0];
