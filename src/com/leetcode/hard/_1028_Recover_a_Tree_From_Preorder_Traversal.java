9f77f8c864b48d9cf649cdf34e33e119eb17b86d
alx <alx@alx.com>
2025-03-19 10:46:51 +0200

    https://leetcode.com/problems/recover-a-tree-from-preorder-traversal

--- src/com/leetcode/hard/_1028_Recover_a_Tree_From_Preorder_Traversal.java
+++ src/com/leetcode/hard/_1028_Recover_a_Tree_From_Preorder_Traversal.java
@@ -34,6 +34,55 @@ public class _1028_Recover_a_Tree_From_Preorder_Traversal {
         return null;
     }
 
+    class Solution {
+        String s;
+        int i = 0;
+        public TreeNode recoverFromPreorder(String traversal) {
+            s = traversal;
+            String num = "";
+            int k = 0;
+            while (k < s.length() && s.charAt(k) != '-') {
+                num += s.charAt(k);
+                k++;
+            }
+            TreeNode root = new TreeNode(Integer.parseInt(num));
+            i = k;
+            dfs(root, 1, true);
+            dfs(root, 1, false);
+            return root;
+        }
+
+        private void dfs(TreeNode t, int cur, boolean dirLeft) {
+            if (s.length() == i) return;
+
+            int j = i;
+            int counter = 0;
+            while (j < s.length() && s.charAt(j) == '-') {
+                j++;
+                counter++;
+            }
+
+            if (counter == cur) {
+                String num = "";
+                int k = j;
+                while (k < s.length() && s.charAt(k) != '-') {
+                    num += s.charAt(k);
+                    k++;
+                }
+                i = k;
+                if (dirLeft) {
+                    t.left = new TreeNode(Integer.parseInt(num));
+                    dfs(t.left, cur + 1, true);
+                    dfs(t.left, cur + 1, false);
+                } else {
+                    t.right = new TreeNode(Integer.parseInt(num));
+                    dfs(t.right, cur + 1, true);
+                    dfs(t.right, cur + 1, false);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         var sol = new _1028_Recover_a_Tree_From_Preorder_Traversal();
         System.out.println(sol.recoverFromPreorder("1-2--3--4-5--6--7"));
