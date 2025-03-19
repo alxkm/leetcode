a557b0637c3dff32567bbd81849c8142001e4286
alx <alx@alx.com>
2025-03-19 10:51:33 +0200

    https://leetcode.com/problems/find-eventual-safe-states

---
+++ src/com/leetcode/medium/graph_hard/_802_Find_Eventual_Safe_States.java
@@ -0,0 +1,40 @@
+package com.leetcode.medium.graph_hard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _802_Find_Eventual_Safe_States {
+    //802. Find Eventual Safe States
+    //https://leetcode.com/problems/find-eventual-safe-states/description/
+
+    int[][] graph;
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        this.graph = graph;
+        int n = graph.length;
+        int[] color = new int[n];
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (dfs(i, color)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    private boolean dfs(int node, int[] color) {
+        if (color[node] > 0) {
+            return color[node] == 2;
+        }
+
+        color[node] = 1;
+        for (int neighbor : graph[node]) {
+            if (!dfs(neighbor, color)) {
+                return false;
+            }
+        }
+
+        color[node] = 2;
+        return true;
+    }
+}
