cb13757b6f776e1ef595b201ab296cbd1d6ac7c5
alx <alx@alx.com>
2025-03-20 14:45:38 +0200

    https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer

--- src/com/leetcode/easy/array/_2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.java
+++ src/com/leetcode/easy/array/_2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer.java
@@ -1,6 +1,7 @@
 package com.leetcode.easy.array;
 
 public class _2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer {
+    //2529. Maximum Count of Positive Integer and Negative Integer
     //https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/
 
     public int maximumCount(int[] nums) {
@@ -11,5 +12,14 @@ public class _2529_Maximum_Count_of_Positive_Integer_and_Negative_Integer {
             else if (n < 0) neg++;
         }
         return Math.max(pos, neg);
+    }
+
+    public int maximumCount1(int[] nums) {
+        int min = 0, max = 0;
+        for (int n: nums) {
+            if (n < 0) min++;
+            else if (n > 0) max++;
+        }
+        return Math.max(min, max);
     }
 }
