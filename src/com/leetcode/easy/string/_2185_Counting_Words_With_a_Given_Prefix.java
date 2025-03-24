b7996ffa9e29a9ea4a95fcd8846cef931d51c227
alx <alx@alx.com>
2025-03-24 12:28:55 +0200

    https://leetcode.com/problems/counting-words-with-a-given-prefix

--- src/com/leetcode/easy/string/_2185_Counting_Words_With_a_Given_Prefix.java
+++ src/com/leetcode/easy/string/_2185_Counting_Words_With_a_Given_Prefix.java
@@ -1,12 +1,23 @@
 package com.leetcode.easy.string;
 
 public class _2185_Counting_Words_With_a_Given_Prefix {
+    //2185. Counting Words With a Given Prefix
+    //https://leetcode.com/problems/counting-words-with-a-given-prefix/description/
+
     public int prefixCount(String[] words, String pref) {
         int counter = 0;
         for (int i = 0; i < words.length; i++) {
             if (words[i].startsWith(pref)) {
                 counter++;
             }
+        }
+        return counter;
+    }
+
+    public int prefixCount1(String[] words, String pref) {
+        int counter = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() >= pref.length() && words[i].startsWith(pref)) counter++;
         }
         return counter;
     }
