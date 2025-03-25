f4403f3ecd5ef68d8b02a6def3fc0cdfbeeecc5c
Oleksandr Klymenko <alexanderklmn@gmail.com>
2025-03-25 11:24:46 +0100

    https://leetcode.com/problems/special-array-i

--- src/com/leetcode/easy/array/_3151_Special_Array_I.java
+++ src/com/leetcode/easy/array/_3151_Special_Array_I.java
@@ -15,6 +15,16 @@ public class _3151_Special_Array_I {
         return true;
     }
 
+    public boolean isArraySpecial1(int[] nums) {
+        if (nums.length == 1) return true;
+        if (nums.length == 2) return ((nums[0] % 2 != 0 && nums[1] % 2 == 0) || (nums[0] % 2 == 0 && nums[1] % 2 != 0));
+        for (int i = 1; i < nums.length; i++) {
+            if ((nums[i - 1] % 2 != 0 && nums[i] % 2 == 0) || (nums[i - 1] % 2 == 0 && nums[i] % 2 != 0)) continue;
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         var sol = new _3151_Special_Array_I();
         System.out.println(sol.isArraySpecial(ArrayUtil.of(2, 1, 4)));
