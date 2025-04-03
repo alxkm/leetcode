bca5803afb56c574da65997e5ce967ebed3131fc
Alex Klymenko <alexanderklmn@gmail.com>
2025-04-03 22:43:07 +0200

    https://leetcode.com/problems/design-a-number-container-system

--- src/com/leetcode/medium/design/_2349_Design_a_Number_Container_System.java
+++ src/com/leetcode/medium/design/_2349_Design_a_Number_Container_System.java
@@ -1,6 +1,8 @@
 package com.leetcode.medium.design;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 
@@ -37,5 +39,32 @@ public class _2349_Design_a_Number_Container_System {
             }
             return -1;
         }
+    }
+
+    public static List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), i);
+        }
+        List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            int next = map.get(s.charAt(i));
+            if (next == i) {
+                l.add(1);
+                continue;
+            }
+            int start = i;
+            while (i <= next && i < s.length()) {
+                next = Math.max(map.get(s.charAt(i)), next);
+                i++;
+            }
+            l.add(i - start);
+            i = next;
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(partitionLabels("ababcbacadefegdehijhklij"));
     }
 }
