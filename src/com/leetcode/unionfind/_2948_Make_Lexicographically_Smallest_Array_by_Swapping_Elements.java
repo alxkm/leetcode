1516601640c7dedae7cc12f79ac48c7927576066
Alex Klymenko <alexanderklmn@gmail.com>
2025-04-07 23:55:12 +0200

    https://leetcode.com/problems/make-lexicographically-smallest-array-by-swapping-elements

---
+++ src/com/leetcode/unionfind/_2948_Make_Lexicographically_Smallest_Array_by_Swapping_Elements.java
@@ -0,0 +1,55 @@
+package com.leetcode.unionfind;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class _2948_Make_Lexicographically_Smallest_Array_by_Swapping_Elements {
+    //2948. Make Lexicographically Smallest Array by Swapping Elements
+    //https://leetcode.com/problems/make-lexicographically-smallest-array-by-swapping-elements/description/
+    //FOREIGN
+
+    class Solution {
+
+        public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+            int[] numsSorted = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) numsSorted[i] = nums[i];
+            Arrays.sort(numsSorted);
+
+            int currGroup = 0;
+            HashMap<Integer, Integer> numToGroup = new HashMap<>();
+            numToGroup.put(numsSorted[0], currGroup);
+
+            HashMap<Integer, LinkedList<Integer>> groupToList = new HashMap<>();
+            groupToList.put(
+                    currGroup,
+                    new LinkedList<Integer>(Arrays.asList(numsSorted[0]))
+            );
+
+            for (int i = 1; i < nums.length; i++) {
+                if (Math.abs(numsSorted[i] - numsSorted[i - 1]) > limit) {
+                    // new group
+                    currGroup++;
+                }
+
+                // assign current element to group
+                numToGroup.put(numsSorted[i], currGroup);
+
+                // add element to sorted group list
+                if (!groupToList.containsKey(currGroup)) {
+                    groupToList.put(currGroup, new LinkedList<Integer>());
+                }
+                groupToList.get(currGroup).add(numsSorted[i]);
+            }
+
+            // iterate through input and overwrite each element with the next element in its corresponding group
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i];
+                int group = numToGroup.get(num);
+                nums[i] = groupToList.get(group).pop();
+            }
+
+            return nums;
+        }
+    }
+}
