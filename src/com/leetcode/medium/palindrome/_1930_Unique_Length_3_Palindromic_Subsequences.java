cb1b766c8f47dd046ed998bf7bc8e9f330393d7d
Alex Klymenko <alexanderklmn@gmail.com>
2025-04-07 23:56:16 +0200

    https://leetcode.com/problems/unique-length-3-palindromic-subsequences

--- src/com/leetcode/medium/palindrome/_1930_Unique_Length_3_Palindromic_Subsequences.java
+++ src/com/leetcode/medium/palindrome/_1930_Unique_Length_3_Palindromic_Subsequences.java
@@ -28,4 +28,33 @@ public class _1930_Unique_Length_3_Palindromic_Subsequences {
 
         return counter;
     }
+
+    class Solution {
+        public int countPalindromicSubsequence(String s) {
+            boolean[][] ar = new boolean[s.length() + 1][26];
+            int counter = 0;
+            ar[s.length()] = new boolean[26];
+            for (int i = s.length() - 1; i >= 0; i--) {
+                boolean[] cur = ar[i + 1].clone();
+                cur[s.charAt(i) - 'a'] = true;
+                ar[i] = cur;
+            }
+
+            boolean[][] set = new boolean[26][26];
+            boolean[] used = new boolean[26];
+            for (int i = 0; i < s.length() - 1; i++) {
+                char ch = s.charAt(i);
+                if (used[ch - 'a']) continue;
+                used[ch - 'a'] = true;
+                for (int j = i + 1; j < s.length() - 1; j++) {
+                    char ch1 = s.charAt(j);
+                    if (set[ch - 'a'][ch1 - 'a']) continue;
+                    set[ch - 'a'][ch1 - 'a'] = true;
+                    boolean[] a = ar[j + 1];
+                    if (a[ch - 'a']) counter++;
+                }
+            }
+            return counter;
+        }
+    }
 }
